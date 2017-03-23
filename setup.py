f3d97fff44c4069feca5cdfc5a91ba583a0c9377
wwmm <wellington.melo@cefet-rj.br>
2017-03-23 20:36:58 -0300

    changed project structure

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='PulseEffects',
-      version='0.3',
+      version='0.4',
       description='Audio effects for Pulseaudio applications',
       url='https://github.com/wwmm/PulseEffects',
       author='wwmm',
