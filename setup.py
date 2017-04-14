dd30172b3c55f9a07e1f3b30ea171218b9adfe53
wwmm <wellington.melo@cefet-rj.br>
2017-04-14 15:46:39 -0300

    updated setup.py version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='PulseEffects',
-      version='1.1.2',
+      version='1.1.3',
       description='Audio effects for Pulseaudio applications',
       url='https://github.com/wwmm/PulseEffects',
       author='wwmm',
