1bc387020afecc9d25cdb7db8b525241700b1406
wwmm <wellington.melo@cefet-rj.br>
2017-04-14 15:43:26 -0300

    saving and loading user presets was finally implemented :-)

--- PulseEffects/application.py
+++ PulseEffects/application.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import configparser
 import os
 
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gio, GLib, Gtk
-
 from PulseEffects.gst import GstEffects
 from PulseEffects.pulse_manager import PulseManager
 
@@ -52,6 +52,10 @@ class Application(Gtk.Application):
             'compressor-user').unpack()
         self.reverb_user = self.settings.get_value('reverb-user').unpack()
         self.eq_band_user = self.settings.get_value('equalizer-user').unpack()
+
+        # creating user presets folder
+        self.user_config_dir = os.path.expanduser('~/.config/PulseEffects')
+        os.makedirs(self.user_config_dir, exist_ok=True)
 
     def do_startup(self):
         Gtk.Application.do_startup(self)
@@ -117,7 +121,9 @@ class Application(Gtk.Application):
             'on_autovolume_enable_state_set':
                 self.on_autovolume_enable_state_set,
             'on_autovolume_time_window_value_changed':
-                self.on_autovolume_time_window_value_changed
+                self.on_autovolume_time_window_value_changed,
+            'on_save_user_preset_clicked': self.on_save_user_preset_clicked,
+            'on_load_user_preset_clicked': self.on_load_user_preset_clicked
         }
 
         main_ui_builder.connect_signals(main_ui_handlers)
@@ -831,6 +837,115 @@ class Application(Gtk.Application):
         value = obj.get_value()
         self.gst.set_eq_band9(value)
         self.save_eq_user(9, value)
+
+    def add_file_filter(self, dialog):
+        file_filter = Gtk.FileFilter()
+        file_filter.set_name("preset")
+        file_filter.add_mime_type("text/plain")
+
+        dialog.add_filter(file_filter)
+
+    def on_save_user_preset_clicked(self, obj):
+        dialog = Gtk.FileChooserDialog("Save Preset", self.window,
+                                       Gtk.FileChooserAction.SAVE,
+                                       (Gtk.STOCK_CANCEL,
+                                        Gtk.ResponseType.CANCEL,
+                                        Gtk.STOCK_SAVE, Gtk.ResponseType.OK))
+
+        dialog.set_current_folder(self.user_config_dir)
+        dialog.set_current_name('user.preset')
+
+        self.add_file_filter(dialog)
+
+        response = dialog.run()
+
+        if response == Gtk.ResponseType.OK:
+            path = dialog.get_filename()
+
+            if not path.endswith(".preset"):
+                path += ".preset"
+
+            output = open(path, 'w')
+
+            config = configparser.ConfigParser()
+
+            limiter = self.settings.get_value('limiter-user')
+
+            config['limiter'] = {'input gain': str(limiter[0]),
+                                 'limit': str(limiter[1]),
+                                 'release time': str(limiter[2])}
+
+            compressor = self.settings.get_value('compressor-user')
+
+            config['compressor'] = {'rms-peak': str(compressor[0]),
+                                    'attack': str(compressor[1]),
+                                    'release': str(compressor[2]),
+                                    'threshold': str(compressor[3]),
+                                    'ratio': str(compressor[4]),
+                                    'knee': str(compressor[5]),
+                                    'makeup': str(compressor[6])}
+
+            reverb = self.settings.get_value('reverb-user')
+
+            config['reverb'] = {'room size': str(reverb[0]),
+                                'damping': str(reverb[1]),
+                                'width': str(reverb[2]),
+                                'level': str(reverb[3])}
+
+            equalizer = self.settings.get_value('equalizer-user')
+
+            config['equalizer'] = {'band0': str(equalizer[0]),
+                                   'band1': str(equalizer[1]),
+                                   'band2': str(equalizer[2]),
+                                   'band3': str(equalizer[3]),
+                                   'band4': str(equalizer[4]),
+                                   'band5': str(equalizer[5]),
+                                   'band6': str(equalizer[6]),
+                                   'band7': str(equalizer[7]),
+                                   'band8': str(equalizer[8]),
+                                   'band9': str(equalizer[9])}
+
+            config.write(output)
+
+        dialog.destroy()
+
+    def on_load_user_preset_clicked(self, obj):
+        dialog = Gtk.FileChooserDialog("Load Preset", self.window,
+                                       Gtk.FileChooserAction.OPEN,
+                                       (Gtk.STOCK_CANCEL,
+                                        Gtk.ResponseType.CANCEL,
+                                        Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+
+        dialog.set_current_folder(self.user_config_dir)
+
+        self.add_file_filter(dialog)
+
+        response = dialog.run()
+
+        if response == Gtk.ResponseType.OK:
+            path = dialog.get_filename()
+
+            config = configparser.ConfigParser()
+
+            config.read(path)
+
+            limiter = dict(config['limiter']).values()
+            limiter = [float(v) for v in limiter]
+            self.apply_limiter_preset(limiter)
+
+            compressor = dict(config['compressor']).values()
+            compressor = [float(v) for v in compressor]
+            self.apply_compressor_preset(compressor)
+
+            reverb = dict(config['reverb']).values()
+            reverb = [float(v) for v in reverb]
+            self.apply_reverb_preset(reverb)
+
+            equalizer = dict(config['equalizer']).values()
+            equalizer = [float(v) for v in equalizer]
+            self.apply_eq_preset(equalizer)
+
+        dialog.destroy()
 
     def onAbout(self, action, parameter):
         builder = Gtk.Builder()
