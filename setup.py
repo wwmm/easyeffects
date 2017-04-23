3e9f276bfdded7ce7cbf9b100a8b4ff567cd4c23
wwmm <wellington.melo@cefet-rj.br>
2017-04-23 00:14:43 -0300

    updated setup.py version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ for lang in langs:
 
 
 setup(name='PulseEffects',
-      version='1.2.1',
+      version='1.2.2',
       description='Audio effects for Pulseaudio applications',
       url='https://github.com/wwmm/PulseEffects',
       author='wwmm',
