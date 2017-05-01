5d84d36e10855651fefb58c6ea621a6a3f819162
wwmm <wellington.melo@cefet-rj.br>
2017-05-01 12:33:57 -0300

    rescaled equal loudness contours presets

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ for lang in langs:
 
 
 setup(name='PulseEffects',
-      version='1.2.5',
+      version='1.2.6',
       description='Audio effects for Pulseaudio applications',
       url='https://github.com/wwmm/PulseEffects',
       author='wwmm',
