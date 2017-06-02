2bf438d57e6b96044ccb16f2f4f043b34d821da8
wwmm <wellington.melo@cefet-rj.br>
2017-06-02 12:20:52 -0300

    sink rate is shown in headerbar

--- PulseEffects/application.py
+++ PulseEffects/application.py
@@ -116,6 +116,10 @@ class Application(Gtk.Application):
 
         buffer_time_obj.set_value(buffer_time)
 
+        # sink rate label
+        sink_rate_label = self.builder.get_object('sink_rate')
+        sink_rate_label.set_text(str(self.pm.default_sink_rate) + ' Hz')
+
         # now that all elements were initialized we set pipeline to ready
         self.gst.set_state('ready')
 
