2b5bc5af561f7abc63fc211c2c885fac636e73f1
wwmm <wellingtonwallace@gmail.com>
2017-06-30 16:35:49 -0300

    decresed crossfade time from 300 ms to 250 ms

--- PulseEffects/application.py
+++ PulseEffects/application.py
@@ -117,7 +117,7 @@ class Application(Gtk.Application):
 
         stack = Gtk.Stack()
         stack.set_transition_type(Gtk.StackTransitionType.CROSSFADE)
-        stack.set_transition_duration(300)
+        stack.set_transition_duration(250)
 
         stack.add_named(sink_inputs_ui, 'sink_inputs')
 
