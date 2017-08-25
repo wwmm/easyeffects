ed2f2e2b4abde00b31c779b310130f86aa6bc92c
wwmm <wellingtonwallace@gmail.com>
2017-08-25 11:53:54 -0300

    updated .gitignore and some changes to indentation

--- meson_post_install.py
+++ meson_post_install.py
@@ -7,12 +7,15 @@ prefix = os.environ.get('MESON_INSTALL_PREFIX', '/usr/local')
 datadir = os.path.join(prefix, 'share')
 
 # Packaging tools define DESTDIR and this isn't needed for them
-if not 'DESTDIR' in os.environ:
-	print('Compiling gsettings schemas...')
-	subprocess.call(['glib-compile-schemas', os.path.join(datadir, 'glib-2.0', 'schemas')])
+if 'DESTDIR' not in os.environ:
+    print('Compiling gsettings schemas...')
+    subprocess.call(
+        ['glib-compile-schemas', os.path.join(datadir, 'glib-2.0', 'schemas')])
 
-	print('Updating icon cache...')
-	subprocess.call(['gtk-update-icon-cache', '-qtf', os.path.join(datadir, 'icons', 'hicolor')])
+    print('Updating icon cache...')
+    subprocess.call(['gtk-update-icon-cache', '-qtf',
+                     os.path.join(datadir, 'icons', 'hicolor')])
 
-	print('Updating desktop database...')
-	subprocess.call(['update-desktop-database', '-q', os.path.join(datadir, 'applications')])
+    print('Updating desktop database...')
+    subprocess.call(['update-desktop-database', '-q',
+                     os.path.join(datadir, 'applications')])
