bd01af8a2a585cf8473418463bb3fe7991243b12
wwmm <wellingtonwallace@gmail.com>
2017-08-31 18:54:46 -0300

    code refactoring

--- PulseEffects/source_output_effects.py
+++ PulseEffects/source_output_effects.py
@@ -1,25 +1,74 @@
 # -*- coding: utf-8 -*-
+import gettext
+import os
 
 import gi
-gi.require_version('Gst', '1.0')
 import numpy as np
-from gi.repository import Gio
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gio, Gtk
+from PulseEffects.compressor import Compressor
+from PulseEffects.equalizer import Equalizer
+from PulseEffects.highpass import Highpass
+from PulseEffects.limiter import Limiter
+from PulseEffects.lowpass import Lowpass
+from PulseEffects.pipeline_base import PipelineBase
+from PulseEffects.reverb import Reverb
 from scipy.interpolate import CubicSpline
-from PulseEffects.source_output_pipeline import SourceOutputPipeline
-from PulseEffects.effects_ui_base import EffectsUiBase
+
+gettext.textdomain('PulseEffects')
+_ = gettext.gettext
 
 
-class SourceOutputEffects(EffectsUiBase, SourceOutputPipeline):
+class SourceOutputEffects(PipelineBase):
 
     def __init__(self, sampling_rate):
         self.settings = Gio.Settings(
             'com.github.wwmm.pulseeffects.sourceoutputs')
 
-        SourceOutputPipeline.__init__(self, sampling_rate)
-        EffectsUiBase.__init__(self, '/ui/source_outputs_plugins.glade',
-                               self.settings)
+        PipelineBase.__init__(self, sampling_rate)
 
-        self.builder.connect_signals(self)
+        self.module_path = os.path.dirname(__file__)
+
+        self.settings = Gio.Settings(
+            'com.github.wwmm.pulseeffects.sourceoutputs')
+
+        self.builder = Gtk.Builder()
+
+        self.builder.add_from_file(self.module_path + '/ui/effects_box.glade')
+
+        self.ui_window = self.builder.get_object('window')
+
+        self.stack = self.builder.get_object('stack')
+
+        self.limiter = Limiter(self.settings)
+        self.compressor = Compressor(self.settings)
+        self.reverb = Reverb(self.settings)
+        self.highpass = Highpass(self.settings)
+        self.lowpass = Lowpass(self.settings)
+        self.equalizer = Equalizer(self.settings)
+
+        self.stack.add_titled(self.limiter.ui_window, 'Limiter',
+                              _('Input Limiter'))
+        self.stack.add_titled(self.compressor.ui_window, 'Compressor',
+                              _('Compressor'))
+        self.stack.add_titled(self.reverb.ui_window, 'Reverb',
+                              _('Reverberation'))
+        self.stack.add_titled(self.highpass.ui_window, 'Highpass',
+                              _('High pass'))
+        self.stack.add_titled(self.lowpass.ui_window, 'Lowpass',
+                              _('Low pass'))
+        self.stack.add_titled(self.equalizer.ui_window, 'Equalizer',
+                              _('Equalizer'))
+
+        # adding effects to the pipeline
+        self.effects_bin.append(self.limiter.bin, self.on_filter_added, None)
+        self.effects_bin.append(self.compressor.bin, self.on_filter_added,
+                                None)
+        self.effects_bin.append(self.reverb.bin, self.on_filter_added, None)
+        self.effects_bin.append(self.highpass.bin, self.on_filter_added, None)
+        self.effects_bin.append(self.lowpass.bin, self.on_filter_added, None)
+        self.effects_bin.append(self.equalizer.bin, self.on_filter_added, None)
+        self.effects_bin.append(self.spectrum, self.on_filter_added, None)
 
     def on_message_element(self, bus, msg):
         plugin = msg.src.get_name()
@@ -27,52 +76,59 @@ class SourceOutputEffects(EffectsUiBase, SourceOutputPipeline):
         if plugin == 'limiter_input_level':
             peak = msg.get_structure().get_value('peak')
 
-            self.ui_update_limiter_input_level(peak)
+            self.limiter.ui_update_limiter_input_level(peak)
         elif plugin == 'limiter_output_level':
             peak = msg.get_structure().get_value('peak')
 
-            self.ui_update_limiter_output_level(peak)
+            self.limiter.ui_update_limiter_output_level(peak)
+        elif plugin == 'autovolume':
+            if self.limiter.autovolume_enabled:
+                peak = msg.get_structure().get_value('peak')
+
+                max_value = max(peak)
+
+                if max_value > self.limiter.autovolume_threshold:
+                    self.limiter.auto_gain(max_value)
         elif plugin == 'compressor_input_level':
             peak = msg.get_structure().get_value('peak')
 
-            self.ui_update_compressor_input_level(peak)
+            self.compressor.ui_update_compressor_input_level(peak)
         elif plugin == 'compressor_output_level':
             peak = msg.get_structure().get_value('peak')
 
-            self.ui_update_compressor_output_level(peak)
-
+            self.compressor.ui_update_compressor_output_level(peak)
         elif plugin == 'reverb_input_level':
             peak = msg.get_structure().get_value('peak')
 
-            self.ui_update_reverb_input_level(peak)
+            self.reverb.ui_update_reverb_input_level(peak)
         elif plugin == 'reverb_output_level':
             peak = msg.get_structure().get_value('peak')
 
-            self.ui_update_reverb_output_level(peak)
+            self.reverb.ui_update_reverb_output_level(peak)
         elif plugin == 'highpass_input_level':
             peak = msg.get_structure().get_value('peak')
 
-            self.ui_update_highpass_input_level(peak)
+            self.highpass.ui_update_highpass_input_level(peak)
         elif plugin == 'highpass_output_level':
             peak = msg.get_structure().get_value('peak')
 
-            self.ui_update_highpass_output_level(peak)
+            self.highpass.ui_update_highpass_output_level(peak)
         elif plugin == 'lowpass_input_level':
             peak = msg.get_structure().get_value('peak')
 
-            self.ui_update_lowpass_input_level(peak)
+            self.lowpass.ui_update_lowpass_input_level(peak)
         elif plugin == 'lowpass_output_level':
             peak = msg.get_structure().get_value('peak')
 
-            self.ui_update_lowpass_output_level(peak)
+            self.lowpass.ui_update_lowpass_output_level(peak)
         elif plugin == 'equalizer_input_level':
             peak = msg.get_structure().get_value('peak')
 
-            self.ui_update_equalizer_input_level(peak)
+            self.equalizer.ui_update_equalizer_input_level(peak)
         elif plugin == 'equalizer_output_level':
             peak = msg.get_structure().get_value('peak')
 
-            self.ui_update_equalizer_output_level(peak)
+            self.equalizer.ui_update_equalizer_output_level(peak)
         elif plugin == 'spectrum':
             magnitudes = msg.get_structure().get_value('magnitude')
 
@@ -92,36 +148,19 @@ class SourceOutputEffects(EffectsUiBase, SourceOutputPipeline):
         return True
 
     def init_ui(self):
-        self.init_limiter_ui()
-        self.init_compressor_ui()
-        self.init_reverb_ui()
-        self.init_highpass_ui()
-        self.init_lowpass_ui()
-        self.init_equalizer_ui()
-
-    def on_eq_flat_response_button_clicked(self, obj):
-        self.apply_eq_preset([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-
-    def on_eq_reset_freqs_button_clicked(self, obj):
-        self.settings.reset('equalizer-freqs')
-        self.init_eq_freq_and_qfactors()
-
-    def on_eq_reset_qfactors_button_clicked(self, obj):
-        self.settings.reset('equalizer-qfactors')
-        self.init_eq_freq_and_qfactors()
+        self.limiter.init_ui()
+        self.compressor.init_ui()
+        self.reverb.init_ui()
+        self.highpass.init_ui()
+        self.lowpass.init_ui()
+        self.equalizer.init_ui()
 
     def reset(self):
-        self.settings.reset('limiter-user')
-        self.settings.reset('compressor-user')
-        self.settings.reset('reverb-user')
-        self.settings.reset('highpass-cutoff')
-        self.settings.reset('highpass-poles')
-        self.settings.reset('lowpass-cutoff')
-        self.settings.reset('lowpass-poles')
-        self.settings.reset('equalizer-input-gain')
-        self.settings.reset('equalizer-output-gain')
-        self.settings.reset('equalizer-user')
-        self.settings.reset('equalizer-freqs')
-        self.settings.reset('equalizer-qfactors')
+        self.limiter.reset()
+        self.compressor.reset()
+        self.reverb.reset()
+        self.highpass.reset()
+        self.lowpass.reset()
+        self.equalizer.reset()
 
         self.init_ui()
