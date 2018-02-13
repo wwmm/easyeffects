c3990a1feb426debccf3c333913afa78825eaa03
Mikhail novosyolov <mikhailnov@dumalogiya.ru>
2018-02-13 05:42:29 +0300

    make some comments in the PPA script

--- debian/build-ppa.sh
+++ debian/build-ppa.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # I use this script to build and publish deb packages in ppa:mikhailnov/pulseeffects (https://launchpad.net/~mikhailnov/+archive/ubuntu/pulseeffects)
 # I publish it to allow other people to use it and make it possible to maintain a new PPA easily in case I stop doing it for some reason
+# I think, it can also be used for maintaining packages in mainline Debian (minor modifications required)
 
 dir0="$(pwd)"
 old_header=$(head -1 ./changelog)
@@ -15,6 +16,9 @@ do
 	dpkg-buildpackage -S -sa
 	sed  -i -re "1s/.*/${old_header}/" ./debian/changelog
 	cd ..
+	# change ppa:mikhailnov/pulseeffects to your PPA at Launchpad.net
+	# you must add your public GPG key to Launchpad
+	# and add your public SSH key to Launchpad before running dput
 	dput -f ppa:mikhailnov/pulseeffects "$(ls -tr pulseeffects_*_source.changes | tail -n 1)" || exit 1
 	cd "${dir0}"
 	sleep 1
