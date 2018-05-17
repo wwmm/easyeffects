8ec7c2353558ecb7063b9bd14f1e0febeb7d5316
wellington wallace <wellingtonwallace@gmail.com>
2018-05-17 19:54:17 -0300

    porting the compressor to c++

---
+++ include/compressor_ui.hpp
@@ -0,0 +1,30 @@
+#ifndef LIMITER_UI_HPP
+#define LIMITER_UI_HPP
+
+#include <gtkmm/adjustment.h>
+#include <gtkmm/grid.h>
+#include "plugin_ui_base.hpp"
+
+class CompressorUi : public Gtk::Grid, public PluginUiBase {
+   public:
+    CompressorUi(BaseObjectType* cobject,
+                 const Glib::RefPtr<Gtk::Builder>& refBuilder,
+                 std::string settings_name);
+    ~CompressorUi();
+
+    static std::shared_ptr<CompressorUi> create(std::string settings_name);
+
+    void reset();
+
+    void on_new_compression(double value);
+
+   private:
+    Gtk::Adjustment* release;
+    Gtk::Grid* controls;
+    Gtk::LevelBar* compression;
+    Gtk::Label* compression_label;
+
+    std::vector<sigc::connection> connections;
+};
+
+#endif
