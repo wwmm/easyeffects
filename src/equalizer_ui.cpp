87843d15b69d020cd2fe189cc2fd6fd5923deb44
Wellington Wallace <wellingtonwallace@gmail.com>
2018-05-20 15:49:05 -0300

    porting the equalizer to c++

--- src/equalizer_ui.cpp
+++ src/equalizer_ui.cpp
@@ -1,4 +1,39 @@
+#include <gtkmm/combobox.h>
 #include "equalizer_ui.hpp"
+
+namespace {
+
+gboolean bandtype_enum_to_int(GValue* value,
+                              GVariant* variant,
+                              gpointer user_data) {
+    auto v = g_variant_get_string(variant, nullptr);
+
+    if (v == std::string("peak")) {
+        g_value_set_int(value, 0);
+    } else if (v == std::string("low-shelf")) {
+        g_value_set_int(value, 1);
+    } else if (v == std::string("high-shelf")) {
+        g_value_set_int(value, 2);
+    }
+
+    return true;
+}
+
+GVariant* int_to_bandtype_enum(const GValue* value,
+                               const GVariantType* expected_type,
+                               gpointer user_data) {
+    int v = g_value_get_int(value);
+
+    if (v == 0) {
+        return g_variant_new_string("peak");
+    } else if (v == 1) {
+        return g_variant_new_string("low-shelf");
+    } else {
+        return g_variant_new_string("high-shelf");
+    }
+}
+
+}  // namespace
 
 EqualizerUi::EqualizerUi(BaseObjectType* cobject,
                          const Glib::RefPtr<Gtk::Builder>& refBuilder,
@@ -20,16 +55,6 @@ EqualizerUi::EqualizerUi(BaseObjectType* cobject,
     auto flag = Gio::SettingsBindFlags::SETTINGS_BIND_DEFAULT;
 
     settings->bind("num-bands", nbands, "value", flag);
-
-    // g_settings_bind_with_mapping(settings->gobj(), "mode", mode->gobj(),
-    //                              "active", G_SETTINGS_BIND_DEFAULT,
-    //                              equalizer_enum_to_int,
-    //                              int_to_equalizer_enum, nullptr, nullptr);
-
-    // connections.push_back(
-    //     settings->signal_changed("num-bands").connect([&](auto key) {
-    //         auto nbands = settings->get_int("num-bands");
-    //     }));
 
     settings->set_boolean("post-messages", true);
 }
@@ -56,13 +81,34 @@ void EqualizerUi::on_nbands_changed() {
         bands_grid->remove(*c);
     }
 
+    auto flag = Gio::SettingsBindFlags::SETTINGS_BIND_DEFAULT;
+
     for (int n = 0; n < N; n++) {
         auto B = Gtk::Builder::create_from_resource(
             "/com/github/wwmm/pulseeffects/equalizer_band.glade");
 
         Gtk::Grid* band_grid;
+        Gtk::ComboBox* band_t;
 
         B->get_widget("band_grid", band_grid);
+        B->get_widget("band_t", band_t);
+
+        auto band_g = (Gtk::Adjustment*)B->get_object("band_g").get();
+        auto band_f = (Gtk::Adjustment*)B->get_object("band_f").get();
+        auto band_w = (Gtk::Adjustment*)B->get_object("band_w").get();
+
+        settings->bind(std::string("band" + std::to_string(n) + "-gain"),
+                       band_g, "value", flag);
+        settings->bind(std::string("band" + std::to_string(n) + "-frequency"),
+                       band_f, "value", flag);
+        settings->bind(std::string("band" + std::to_string(n) + "-width"),
+                       band_w, "value", flag);
+
+        g_settings_bind_with_mapping(
+            settings->gobj(),
+            std::string("band" + std::to_string(n) + "-type").c_str(),
+            band_t->gobj(), "active", G_SETTINGS_BIND_DEFAULT,
+            bandtype_enum_to_int, int_to_bandtype_enum, nullptr, nullptr);
 
         bands_grid->add(*band_grid);
     }
