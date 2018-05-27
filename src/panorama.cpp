d0c22e860996704704684b89378aef83b52484fa
Wellington Wallace <wellingtonwallace@gmail.com>
2018-05-27 19:32:55 -0300

    improving the pipeline

--- src/panorama.cpp
+++ src/panorama.cpp
@@ -61,16 +61,24 @@ Panorama::Panorama(std::string tag, std::string schema)
     }
 
     if (is_installed) {
-        bin = gst_insert_bin_new("panorama_bin");
+        bin = gst_bin_new("panorama_bin");
 
         auto in_level =
             gst_element_factory_make("level", "panorama_input_level");
         auto out_level =
             gst_element_factory_make("level", "panorama_output_level");
 
-        gst_insert_bin_append(GST_INSERT_BIN(bin), in_level, nullptr, nullptr);
-        gst_insert_bin_append(GST_INSERT_BIN(bin), panorama, nullptr, nullptr);
-        gst_insert_bin_append(GST_INSERT_BIN(bin), out_level, nullptr, nullptr);
+        gst_bin_add_many(GST_BIN(bin), in_level, panorama, out_level, nullptr);
+        gst_element_link_many(in_level, panorama, out_level, nullptr);
+
+        auto pad_sink = gst_element_get_static_pad(in_level, "sink");
+        auto pad_src = gst_element_get_static_pad(out_level, "src");
+
+        gst_element_add_pad(bin, gst_ghost_pad_new("sink", pad_sink));
+        gst_element_add_pad(bin, gst_ghost_pad_new("src", pad_src));
+
+        gst_object_unref(GST_OBJECT(pad_sink));
+        gst_object_unref(GST_OBJECT(pad_src));
 
         g_object_set(panorama, "method", 0, nullptr);  // Psychoacoustic Panning
 
