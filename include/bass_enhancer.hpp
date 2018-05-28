071578746e62e233c28d974369e88e5fb461fe0c
Wellington Wallace <wellingtonwallace@gmail.com>
2018-05-28 12:08:06 -0300

    improving plugins enable/disable

--- include/bass_enhancer.hpp
+++ include/bass_enhancer.hpp
@@ -1,29 +1,22 @@
 #ifndef BASS_ENHANCER_HPP
 #define BASS_ENHANCER_HPP
 
-#include <gio/gio.h>
-#include <gst/gst.h>
 #include <sigc++/sigc++.h>
 #include <array>
-#include <iostream>
+#include "plugin_base.hpp"
 
-class BassEnhancer {
+class BassEnhancer : public PluginBase {
    public:
     BassEnhancer(std::string tag, std::string schema);
     ~BassEnhancer();
 
-    std::string log_tag, name = "bass_enhancer";
-    GstElement *plugin, *bin, *bass_enhancer;
+    GstElement* bass_enhancer;
 
     sigc::connection harmonics_connection;
 
     sigc::signal<void, double> harmonics;
 
    private:
-    bool is_installed;
-
-    GSettings* settings;
-
     void bind_to_gsettings();
 };
 
--- src/bass_enhancer.cpp
+++ src/bass_enhancer.cpp
@@ -1,48 +1,8 @@
 #include <glibmm/main.h>
-#include <gst/insertbin/gstinsertbin.h>
 #include "bass_enhancer.hpp"
 #include "util.hpp"
 
 namespace {
-
-void on_state_changed(GSettings* settings, gchar* key, BassEnhancer* l) {
-    auto enable = g_settings_get_boolean(settings, key);
-    auto plugin = gst_bin_get_by_name(GST_BIN(l->plugin), "bass_enhancer_bin");
-
-    if (enable) {
-        if (!plugin) {
-            gst_insert_bin_append(
-                GST_INSERT_BIN(l->plugin), l->bin,
-                [](auto bin, auto elem, auto success, auto d) {
-                    auto l = static_cast<BassEnhancer*>(d);
-
-                    if (success) {
-                        util::debug(l->log_tag + "bass_enhancer enabled");
-                    } else {
-                        util::debug(l->log_tag +
-                                    "failed to enable the bass_enhancer");
-                    }
-                },
-                l);
-        }
-    } else {
-        if (plugin) {
-            gst_insert_bin_remove(
-                GST_INSERT_BIN(l->plugin), l->bin,
-                [](auto bin, auto elem, auto success, auto d) {
-                    auto l = static_cast<BassEnhancer*>(d);
-
-                    if (success) {
-                        util::debug(l->log_tag + "bass_enhancer disabled");
-                    } else {
-                        util::debug(l->log_tag +
-                                    "failed to disable the bass_enhancer");
-                    }
-                },
-                l);
-        }
-    }
-}
 
 void on_post_messages_changed(GSettings* settings,
                               gchar* key,
@@ -70,42 +30,35 @@ void on_post_messages_changed(GSettings* settings,
 }  // namespace
 
 BassEnhancer::BassEnhancer(std::string tag, std::string schema)
-    : log_tag(tag), settings(g_settings_new(schema.c_str())) {
+    : PluginBase(tag, "bass_enhancer", schema) {
     bass_enhancer = gst_element_factory_make(
         "calf-sourceforge-net-plugins-BassEnhancer", nullptr);
 
-    plugin = gst_insert_bin_new("bass_enhancer_plugin");
-
-    if (bass_enhancer != nullptr) {
-        is_installed = true;
-    } else {
-        is_installed = false;
-
-        util::warning("BassEnhancer plugin was not found!");
-    }
-
-    if (is_installed) {
-        bin = gst_insert_bin_new("bass_enhancer_bin");
+    if (is_installed(bass_enhancer)) {
+        bin = gst_bin_new("bass_enhancer_bin");
 
         auto in_level =
             gst_element_factory_make("level", "bass_enhancer_input_level");
         auto out_level =
             gst_element_factory_make("level", "bass_enhancer_output_level");
-        auto audioconvert = gst_element_factory_make("audioconvert", nullptr);
 
-        gst_insert_bin_append(GST_INSERT_BIN(bin), in_level, nullptr, nullptr);
-        gst_insert_bin_append(GST_INSERT_BIN(bin), audioconvert, nullptr,
-                              nullptr);
-        gst_insert_bin_append(GST_INSERT_BIN(bin), bass_enhancer, nullptr,
-                              nullptr);
-        gst_insert_bin_append(GST_INSERT_BIN(bin), out_level, nullptr, nullptr);
+        gst_bin_add_many(GST_BIN(bin), in_level, bass_enhancer, out_level,
+                         nullptr);
+        gst_element_link_many(in_level, bass_enhancer, out_level, nullptr);
+
+        auto pad_sink = gst_element_get_static_pad(in_level, "sink");
+        auto pad_src = gst_element_get_static_pad(out_level, "src");
+
+        gst_element_add_pad(bin, gst_ghost_pad_new("sink", pad_sink));
+        gst_element_add_pad(bin, gst_ghost_pad_new("src", pad_src));
+
+        gst_object_unref(GST_OBJECT(pad_sink));
+        gst_object_unref(GST_OBJECT(pad_src));
 
         g_object_set(bass_enhancer, "bypass", false, nullptr);
 
         bind_to_gsettings();
 
-        g_signal_connect(settings, "changed::state",
-                         G_CALLBACK(on_state_changed), this);
         g_signal_connect(settings, "changed::post-messages",
                          G_CALLBACK(on_post_messages_changed), this);
 
