7696ed5f30e56178a9bf798c581e1467941ca487
Wellington Wallace <wellingtonwallace@gmail.com>
2018-05-28 16:12:27 -0300

    improving plugins enable/disable

--- include/webrtc.hpp
+++ include/webrtc.hpp
@@ -1,25 +1,18 @@
 #ifndef WEBRTC_HPP
 #define WEBRTC_HPP
 
-#include <gio/gio.h>
-#include <gst/gst.h>
-#include <iostream>
+#include "plugin_base.hpp"
 
-class Webrtc {
+class Webrtc : public PluginBase {
    public:
     Webrtc(std::string tag, std::string schema);
     ~Webrtc();
 
-    std::string log_tag, name = "webrtc";
-    GstElement *plugin, *bin, *identity_in, *identity_out, *webrtc, *probe_bin,
-        *probe_src;
+    GstElement *webrtc, *probe_bin, *probe_src;
 
     void set_probe_src_device(std::string name);
 
    private:
-    GSettings* settings;
-
-    bool is_installed(GstElement* e);
     void build_probe_bin();
     void build_dsp_bin();
     void bind_to_gsettings();
