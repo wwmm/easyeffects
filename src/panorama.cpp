eb7e2cd5073cf66a6550a9e9169033382ef1a861
Wellington Wallace <wellingtonwallace@gmail.com>
2018-05-27 22:08:49 -0300

    improving plugins enable/disable

--- src/panorama.cpp
+++ src/panorama.cpp
@@ -1,5 +1,6 @@
 #include <glibmm/main.h>
 #include <gst/insertbin/gstinsertbin.h>
+#include <mutex>
 #include "panorama.hpp"
 #include "util.hpp"
 
@@ -7,40 +8,59 @@ namespace {
 
 void on_state_changed(GSettings* settings, gchar* key, Panorama* l) {
     auto enable = g_settings_get_boolean(settings, key);
-    auto plugin = gst_bin_get_by_name(GST_BIN(l->plugin), "panorama_bin");
 
     if (enable) {
-        if (!plugin) {
-            gst_insert_bin_append(
-                GST_INSERT_BIN(l->plugin), l->bin,
-                [](auto bin, auto elem, auto success, auto d) {
-                    auto l = static_cast<Panorama*>(d);
+        gst_pad_add_probe(
+            gst_element_get_static_pad(l->identity_in, "sink"),
+            GST_PAD_PROBE_TYPE_IDLE,
+            [](auto pad, auto info, auto d) {
+                auto l = static_cast<Panorama*>(d);
 
-                    if (success) {
-                        util::debug(l->log_tag + "panorama enabled");
-                    } else {
-                        util::debug(l->log_tag +
-                                    "failed to enable the panorama");
-                    }
-                },
-                l);
-        }
+                auto plugin =
+                    gst_bin_get_by_name(GST_BIN(l->plugin), "panorama_bin");
+
+                if (!plugin) {
+                    gst_element_unlink(l->identity_in, l->identity_out);
+
+                    gst_bin_add(GST_BIN(l->plugin), l->bin);
+
+                    gst_element_sync_state_with_parent(l->identity_in);
+                    gst_element_sync_state_with_parent(l->bin);
+                    gst_element_sync_state_with_parent(l->identity_out);
+
+                    gst_element_link_many(l->identity_in, l->bin,
+                                          l->identity_out, nullptr);
+                }
+
+                return GST_PAD_PROBE_REMOVE;
+            },
+            l, nullptr);
     } else {
-        if (plugin) {
-            gst_insert_bin_remove(
-                GST_INSERT_BIN(l->plugin), l->bin,
-                [](auto bin, auto elem, auto success, auto d) {
-                    auto l = static_cast<Panorama*>(d);
+        gst_pad_add_probe(
+            gst_element_get_static_pad(l->identity_in, "sink"),
+            GST_PAD_PROBE_TYPE_IDLE,
+            [](auto pad, auto info, auto d) {
+                auto l = static_cast<Panorama*>(d);
 
-                    if (success) {
-                        util::debug(l->log_tag + "panorama disabled");
-                    } else {
-                        util::debug(l->log_tag +
-                                    "failed to disable the panorama");
-                    }
-                },
-                l);
-        }
+                auto plugin =
+                    gst_bin_get_by_name(GST_BIN(l->plugin), "panorama_bin");
+
+                if (plugin) {
+                    gst_element_unlink_many(l->identity_in, l->bin,
+                                            l->identity_out, nullptr);
+
+                    gst_bin_remove(GST_BIN(l->plugin), l->bin);
+                    gst_element_set_state(l->bin, GST_STATE_NULL);
+
+                    gst_element_sync_state_with_parent(l->identity_in);
+                    gst_element_sync_state_with_parent(l->identity_out);
+
+                    gst_element_link(l->identity_in, l->identity_out);
+                }
+
+                return GST_PAD_PROBE_REMOVE;
+            },
+            l, nullptr);
     }
 }
 
@@ -50,22 +70,21 @@ Panorama::Panorama(std::string tag, std::string schema)
     : log_tag(tag), settings(g_settings_new(schema.c_str())) {
     panorama = gst_element_factory_make("audiopanorama", nullptr);
 
-    plugin = gst_insert_bin_new("panorama_plugin");
+    // plugin = gst_insert_bin_new("panorama_plugin");
 
-    // plugin = gst_bin_new("panorama_plugin");
-    // identity_in = gst_element_factory_make("identity", nullptr);
-    // identity_out = gst_element_factory_make("identity", nullptr);
-    //
-    // gst_bin_add_many(GST_BIN(plugin), identity_in, identity_out, nullptr);
-    // gst_element_link_many(identity_in, identity_out, nullptr);
-    //
-    // gst_element_add_pad(
-    //     plugin, gst_ghost_pad_new(
-    //                 "sink", gst_element_get_static_pad(identity_in,
-    //                 "sink")));
-    // gst_element_add_pad(
-    //     plugin, gst_ghost_pad_new(
-    //                 "src", gst_element_get_static_pad(identity_out, "src")));
+    plugin = gst_bin_new("panorama_plugin");
+    identity_in = gst_element_factory_make("identity", nullptr);
+    identity_out = gst_element_factory_make("identity", nullptr);
+
+    gst_bin_add_many(GST_BIN(plugin), identity_in, identity_out, nullptr);
+    gst_element_link_many(identity_in, identity_out, nullptr);
+
+    gst_element_add_pad(
+        plugin, gst_ghost_pad_new(
+                    "sink", gst_element_get_static_pad(identity_in, "sink")));
+    gst_element_add_pad(
+        plugin, gst_ghost_pad_new(
+                    "src", gst_element_get_static_pad(identity_out, "src")));
 
     if (panorama != nullptr) {
         is_installed = true;
