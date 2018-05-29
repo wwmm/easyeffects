384e75d8fd896ff7f36723a3b415180f0dabbcc5
Wellington Wallace <wellingtonwallace@gmail.com>
2018-05-28 22:35:56 -0300

    improving plugins enable/disable

--- src/plugin_base.cpp
+++ src/plugin_base.cpp
@@ -3,61 +3,130 @@
 
 namespace {
 
+GstPadProbeReturn event_add(GstPad* pad,
+                            GstPadProbeInfo* info,
+                            gpointer user_data) {
+    auto l = static_cast<PluginBase*>(user_data);
+
+    gst_pad_remove_probe(pad, GST_PAD_PROBE_INFO_ID(info));
+
+    gst_element_unlink(l->identity_in, l->identity_out);
+
+    gst_bin_add(GST_BIN(l->plugin), l->bin);
+
+    gst_bin_sync_children_states(GST_BIN(l->plugin));
+
+    gst_element_link_many(l->identity_in, l->bin, l->identity_out, nullptr);
+
+    util::debug(l->log_tag + l->name + " enabled");
+
+    return GST_PAD_PROBE_DROP;
+}
+
+GstPadProbeReturn event_remove(GstPad* pad,
+                               GstPadProbeInfo* info,
+                               gpointer user_data) {
+    auto l = static_cast<PluginBase*>(user_data);
+
+    gst_pad_remove_probe(pad, GST_PAD_PROBE_INFO_ID(info));
+
+    gst_element_unlink_many(l->identity_in, l->bin, l->identity_out, nullptr);
+
+    gst_bin_remove(GST_BIN(l->plugin), l->bin);
+    gst_element_set_state(l->bin, GST_STATE_NULL);
+
+    gst_element_sync_state_with_parent(l->identity_in);
+    gst_element_sync_state_with_parent(l->identity_out);
+
+    gst_element_link(l->identity_in, l->identity_out);
+
+    util::debug(l->log_tag + l->name + " disabled");
+
+    return GST_PAD_PROBE_DROP;
+}
+
 void on_state_changed(GSettings* settings, gchar* key, PluginBase* l) {
     auto enable = g_settings_get_boolean(settings, key);
 
     if (enable) {
         gst_pad_add_probe(
             gst_element_get_static_pad(l->identity_in, "src"),
-            GST_PAD_PROBE_TYPE_IDLE,
+            GST_PAD_PROBE_TYPE_BLOCK_DOWNSTREAM,
             [](auto pad, auto info, auto d) {
+                gst_pad_remove_probe(pad, GST_PAD_PROBE_INFO_ID(info));
+
                 auto l = static_cast<PluginBase*>(d);
 
                 auto plugin = gst_bin_get_by_name(
                     GST_BIN(l->plugin), std::string(l->name + "_bin").c_str());
 
                 if (!plugin) {
-                    gst_element_unlink(l->identity_in, l->identity_out);
+                    auto srcpad =
+                        gst_element_get_static_pad(l->identity_out, "src");
 
-                    gst_bin_add(GST_BIN(l->plugin), l->bin);
+                    auto probe_type = static_cast<GstPadProbeType>(
+                        GST_PAD_PROBE_TYPE_BLOCK |
+                        GST_PAD_PROBE_TYPE_EVENT_DOWNSTREAM);
 
-                    gst_bin_sync_children_states(GST_BIN(l->plugin));
+                    gst_pad_add_probe(srcpad, probe_type, event_add, l,
+                                      nullptr);
 
-                    gst_element_link_many(l->identity_in, l->bin,
-                                          l->identity_out, nullptr);
+                    gst_object_unref(srcpad);
 
-                    util::debug(l->log_tag + l->name + " enabled");
+                    /* push EOS into the element, the probe will be fired when
+                     * the EOS leaves the effect and it has thus drained all of
+                     * its data
+                     */
+
+                    auto sinkpad =
+                        gst_element_get_static_pad(l->identity_out, "sink");
+
+                    gst_pad_send_event(sinkpad, gst_event_new_eos());
+
+                    gst_object_unref(sinkpad);
                 }
 
-                return GST_PAD_PROBE_REMOVE;
+                return GST_PAD_PROBE_OK;
             },
             l, nullptr);
+
     } else {
         gst_pad_add_probe(
             gst_element_get_static_pad(l->identity_in, "src"),
-            GST_PAD_PROBE_TYPE_IDLE,
+            GST_PAD_PROBE_TYPE_BLOCK_DOWNSTREAM,
             [](auto pad, auto info, auto d) {
+                gst_pad_remove_probe(pad, GST_PAD_PROBE_INFO_ID(info));
+
                 auto l = static_cast<PluginBase*>(d);
 
                 auto plugin = gst_bin_get_by_name(
                     GST_BIN(l->plugin), std::string(l->name + "_bin").c_str());
 
                 if (plugin) {
-                    gst_element_unlink_many(l->identity_in, l->bin,
-                                            l->identity_out, nullptr);
+                    auto srcpad = gst_element_get_static_pad(l->bin, "src");
 
-                    gst_bin_remove(GST_BIN(l->plugin), l->bin);
-                    gst_element_set_state(l->bin, GST_STATE_NULL);
+                    auto probe_type = static_cast<GstPadProbeType>(
+                        GST_PAD_PROBE_TYPE_BLOCK |
+                        GST_PAD_PROBE_TYPE_EVENT_DOWNSTREAM);
 
-                    gst_element_sync_state_with_parent(l->identity_in);
-                    gst_element_sync_state_with_parent(l->identity_out);
+                    gst_pad_add_probe(srcpad, probe_type, event_remove, l,
+                                      nullptr);
 
-                    gst_element_link(l->identity_in, l->identity_out);
+                    gst_object_unref(srcpad);
 
-                    util::debug(l->log_tag + l->name + " disabled");
+                    /* push EOS into the element, the probe will be fired when
+                     * the EOS leaves the effect and it has thus drained all of
+                     * its data
+                     */
+
+                    auto sinkpad = gst_element_get_static_pad(l->bin, "sink");
+
+                    gst_pad_send_event(sinkpad, gst_event_new_eos());
+
+                    gst_object_unref(sinkpad);
                 }
 
-                return GST_PAD_PROBE_REMOVE;
+                return GST_PAD_PROBE_OK;
             },
             l, nullptr);
     }
