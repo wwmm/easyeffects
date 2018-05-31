ae74d8c34c55507ff8d2ea63b0ac4f2d15cc649b
Wellington Wallace <wellingtonwallace@gmail.com>
2018-05-31 01:17:42 -0300

    porting the presets manager to c++

--- src/equalizer_preset.cpp
+++ src/equalizer_preset.cpp
@@ -19,6 +19,18 @@ void EqualizerPreset::save(boost::property_tree::ptree& root,
         root.put(section + ".equalizer.band" + std::to_string(n) + ".gain",
                  settings->get_double(
                      std::string("band" + std::to_string(n) + "-gain")));
+
+        root.put(section + ".equalizer.band" + std::to_string(n) + ".frequency",
+                 settings->get_double(
+                     std::string("band" + std::to_string(n) + "-frequency")));
+
+        root.put(section + ".equalizer.band" + std::to_string(n) + ".width",
+                 settings->get_double(
+                     std::string("band" + std::to_string(n) + "-width")));
+
+        root.put(section + ".equalizer.band" + std::to_string(n) + ".type",
+                 settings->get_string(
+                     std::string("band" + std::to_string(n) + "-type")));
     }
 }
 
@@ -29,44 +41,44 @@ void EqualizerPreset::load(boost::property_tree::ptree& root,
                           root.get<bool>(section + ".equalizer.state",
                                          get_default<bool>(settings, "state")));
 
-    settings->set_string(
-        "detection",
-        root.get<std::string>(section + ".equalizer.detection",
-                              get_default<std::string>(settings, "detection")));
+    int nbands = root.get<int>(section + ".equalizer.num-bands",
+                               get_default<int>(settings, "num-bands"));
 
-    settings->set_string(
-        "stereo-link", root.get<std::string>(
-                           section + ".equalizer.stereo-link",
-                           get_default<std::string>(settings, "stereo-link")));
+    settings->set_int("num-bands", nbands);
 
-    settings->set_double(
-        "mix", root.get<double>(section + ".equalizer.mix",
-                                get_default<double>(settings, "mix")));
+    for (int n = 0; n < nbands; n++) {
+        settings->set_double(
+            std::string("band" + std::to_string(n) + "-gain"),
+            root.get<double>(
+                section + ".equalizer.band" + std::to_string(n) + ".gain",
+                get_default<double>(
+                    settings,
+                    std::string("band" + std::to_string(n) + "-gain"))));
 
-    settings->set_double(
-        "attack", root.get<double>(section + ".equalizer.attack",
-                                   get_default<double>(settings, "attack")));
+        settings->set_double(
+            std::string("band" + std::to_string(n) + "-frequency"),
+            root.get<double>(
+                section + ".equalizer.band" + std::to_string(n) + ".frequency",
+                get_default<double>(
+                    settings,
+                    std::string("band" + std::to_string(n) + "-frequency"))));
 
-    settings->set_double(
-        "release", root.get<double>(section + ".equalizer.release",
-                                    get_default<double>(settings, "release")));
+        settings->set_double(
+            std::string("band" + std::to_string(n) + "-width"),
+            root.get<double>(
+                section + ".equalizer.band" + std::to_string(n) + ".width",
+                get_default<double>(
+                    settings,
+                    std::string("band" + std::to_string(n) + "-width"))));
 
-    settings->set_double(
-        "threshold",
-        root.get<double>(section + ".equalizer.threshold",
-                         get_default<double>(settings, "threshold")));
-
-    settings->set_double(
-        "ratio", root.get<double>(section + ".equalizer.ratio",
-                                  get_default<double>(settings, "ratio")));
-
-    settings->set_double(
-        "knee", root.get<double>(section + ".equalizer.knee",
-                                 get_default<double>(settings, "knee")));
-
-    settings->set_double(
-        "makeup", root.get<double>(section + ".equalizer.makeup",
-                                   get_default<double>(settings, "makeup")));
+        settings->set_string(
+            std::string("band" + std::to_string(n) + "-type"),
+            root.get<std::string>(
+                section + ".equalizer.band" + std::to_string(n) + ".type",
+                get_default<std::string>(
+                    settings,
+                    std::string("band" + std::to_string(n) + "-type"))));
+    }
 }
 
 void EqualizerPreset::write(boost::property_tree::ptree& root) {
