fb3f756a74f34ef3f658d27a2d7aa996b9d43a86
Wellington Wallace <wellingtonwallace@gmail.com>
2018-06-02 12:45:53 -0300

    porting the test signals to c++

--- include/calibration_ui.hpp
+++ include/calibration_ui.hpp
@@ -2,6 +2,8 @@
 #define CALIBRATION_UI_HPP
 
 #include <gtkmm/builder.h>
+#include <gtkmm/drawingarea.h>
+#include <gtkmm/stack.h>
 #include <gtkmm/window.h>
 
 class CalibrationUi : public Gtk::Window {
@@ -15,6 +17,27 @@ class CalibrationUi : public Gtk::Window {
 
    private:
     Glib::RefPtr<Gtk::Builder> builder;
+
+    Gtk::Stack* stack;
+    Gtk::DrawingArea* spectrum;
+
+    sigc::connection spectrum_connection;
+
+    bool mouse_inside;
+    double mouse_intensity, mouse_freq;
+    std::vector<float> spectrum_mag;
+
+    void on_new_spectrum(const std::vector<float>& magnitudes);
+
+    bool on_spectrum_draw(const Cairo::RefPtr<Cairo::Context>& ctx);
+
+    bool on_spectrum_enter_notify_event(GdkEventCrossing* event);
+
+    bool on_spectrum_leave_notify_event(GdkEventCrossing* event);
+
+    bool on_spectrum_motion_notify_event(GdkEventMotion* event);
+
+    void on_stack_visible_child_changed();
 };
 
 #endif
--- src/calibration_ui.cpp
+++ src/calibration_ui.cpp
@@ -1,8 +1,27 @@
 #include "calibration_ui.hpp"
+#include "util.hpp"
 
 CalibrationUi::CalibrationUi(BaseObjectType* cobject,
                              const Glib::RefPtr<Gtk::Builder>& refBuilder)
-    : Gtk::Window(cobject), builder(refBuilder) {}
+    : Gtk::Window(cobject), builder(refBuilder) {
+    // loading glade widgets
+
+    builder->get_widget("stack", stack);
+    builder->get_widget("spectrum", spectrum);
+
+    spectrum->signal_draw().connect(
+        sigc::mem_fun(*this, &CalibrationUi::on_spectrum_draw));
+    spectrum->signal_enter_notify_event().connect(
+        sigc::mem_fun(*this, &CalibrationUi::on_spectrum_enter_notify_event));
+    spectrum->signal_leave_notify_event().connect(
+        sigc::mem_fun(*this, &CalibrationUi::on_spectrum_leave_notify_event));
+    spectrum->signal_motion_notify_event().connect(
+        sigc::mem_fun(*this, &CalibrationUi::on_spectrum_motion_notify_event));
+
+    stack->connect_property_changed(
+        "visible-child",
+        sigc::mem_fun(*this, &CalibrationUi::on_stack_visible_child_changed));
+}
 
 CalibrationUi::~CalibrationUi() {}
 
@@ -15,4 +34,113 @@ std::unique_ptr<CalibrationUi> CalibrationUi::create() {
     builder->get_widget_derived("window", window);
 
     return std::unique_ptr<CalibrationUi>(window);
+}
+
+void CalibrationUi::on_new_spectrum(const std::vector<float>& magnitudes) {
+    spectrum_mag = magnitudes;
+
+    spectrum->queue_draw();
+}
+
+bool CalibrationUi::on_spectrum_draw(const Cairo::RefPtr<Cairo::Context>& ctx) {
+    ctx->paint();
+
+    auto n_bars = spectrum_mag.size();
+
+    if (n_bars > 0) {
+        auto allocation = spectrum->get_allocation();
+        auto width = allocation.get_width();
+        auto height = allocation.get_height();
+        auto n_bars = spectrum_mag.size();
+        auto x = util::linspace(0, width, n_bars);
+
+        for (uint n = 0; n < n_bars; n++) {
+            auto bar_height = spectrum_mag[n] * height;
+
+            ctx->rectangle(x[n], height - bar_height, width / n_bars,
+                           bar_height);
+        }
+
+        auto color = Gdk::RGBA();
+        auto style_ctx = spectrum->get_style_context();
+
+        style_ctx->lookup_color("theme_selected_bg_color", color);
+
+        ctx->set_source_rgba(color.get_red(), color.get_green(),
+                             color.get_blue(), 1.0);
+
+        ctx->set_line_width(1.1);
+        ctx->stroke();
+
+        if (mouse_inside) {
+            std::ostringstream msg;
+
+            msg.precision(0);
+            msg << std::fixed << mouse_freq << " Hz, ";
+            msg << std::fixed << mouse_intensity << " dB";
+
+            Pango::FontDescription font;
+            font.set_family("Monospace");
+            font.set_weight(Pango::WEIGHT_BOLD);
+
+            int text_width;
+            int text_height;
+            auto layout = create_pango_layout(msg.str());
+            layout->set_font_description(font);
+            layout->get_pixel_size(text_width, text_height);
+
+            ctx->move_to(width - text_width, 0);
+
+            layout->show_in_cairo_context(ctx);
+        }
+    }
+
+    return false;
+}
+
+bool CalibrationUi::on_spectrum_enter_notify_event(GdkEventCrossing* event) {
+    mouse_inside = true;
+    return false;
+}
+
+bool CalibrationUi::on_spectrum_leave_notify_event(GdkEventCrossing* event) {
+    mouse_inside = false;
+    return false;
+}
+
+bool CalibrationUi::on_spectrum_motion_notify_event(GdkEventMotion* event) {
+    auto allocation = spectrum->get_allocation();
+
+    auto width = allocation.get_width();
+    auto height = allocation.get_height();
+
+    // frequency axis is logarithmic
+    // 20 Hz = 10^(1.3), 20000 Hz = 10^(4.3)
+
+    mouse_freq = pow(10, 1.3 + event->x * 3.0 / width);
+
+    // intensity scale is in decibel
+    // minimum intensity is -120 dB and maximum is 0 dB
+
+    mouse_intensity = -event->y * 120 / height;
+
+    return false;
+}
+
+void CalibrationUi::on_stack_visible_child_changed() {
+    auto name = stack->get_visible_child_name();
+
+    if (name == std::string("signals")) {
+        spectrum_connection.disconnect();
+
+        // spectrum_connection = app->sie->new_spectrum.connect(
+        //     sigc::mem_fun(*this, &ApplicationUi::on_new_spectrum));
+    } else if (name == std::string("microphone")) {
+        spectrum_connection.disconnect();
+
+        // spectrum_connection = app->soe->new_spectrum.connect(
+        //     sigc::mem_fun(*this, &ApplicationUi::on_new_spectrum));
+    }
+
+    // clear_spectrum();
 }
