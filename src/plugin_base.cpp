386a546d9c42a634c8ebd570446fb70a2ffe1760
Wellington Wallace <wellingtonwallace@gmail.com>
2018-06-11 13:01:51 -0300

    improved dynamic pipeline

--- src/plugin_base.cpp
+++ src/plugin_base.cpp
@@ -1,9 +1,10 @@
-#include <chrono>
-#include <thread>
+#include <mutex>
 #include "plugin_base.hpp"
 #include "util.hpp"
 
 namespace {
+
+std::mutex mtx;
 
 void on_state_changed(GSettings* settings, gchar* key, PluginBase* l) {
     bool enable = g_settings_get_boolean(settings, key);
@@ -71,83 +72,63 @@ bool PluginBase::is_installed(GstElement* e) {
 }
 
 void PluginBase::enable() {
-    auto b = gst_bin_get_by_name(GST_BIN(plugin),
-                                 std::string(name + "_bin").c_str());
+    gst_pad_add_probe(
+        gst_element_get_static_pad(identity_in, "src"),
+        GST_PAD_PROBE_TYPE_BLOCK_DOWNSTREAM,
+        [](auto pad, auto info, auto d) {
+            auto l = static_cast<PluginBase*>(d);
 
-    if (!b) {
-        bool changing;
+            std::lock_guard<std::mutex> lock(mtx);
 
-        do {
-            changing = changing_pipeline.exchange(true);
+            auto b = gst_bin_get_by_name(GST_BIN(l->plugin),
+                                         std::string(l->name + "_bin").c_str());
 
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        } while (changing);
+            if (!b) {
+                gst_element_unlink(l->identity_in, l->identity_out);
 
-        if (!changing) {
-            gst_pad_add_probe(
-                gst_element_get_static_pad(identity_in, "src"),
-                GST_PAD_PROBE_TYPE_IDLE,
-                [](auto pad, auto info, auto d) {
-                    auto l = static_cast<PluginBase*>(d);
+                gst_bin_add(GST_BIN(l->plugin), l->bin);
 
-                    gst_element_unlink(l->identity_in, l->identity_out);
+                gst_element_link_many(l->identity_in, l->bin, l->identity_out,
+                                      nullptr);
 
-                    gst_bin_add(GST_BIN(l->plugin), l->bin);
+                gst_bin_sync_children_states(GST_BIN(l->plugin));
 
-                    gst_element_link_many(l->identity_in, l->bin,
-                                          l->identity_out, nullptr);
+                util::debug(l->log_tag + l->name + " enabled");
+            }
 
-                    gst_bin_sync_children_states(GST_BIN(l->plugin));
-
-                    util::debug(l->log_tag + l->name + " enabled");
-
-                    l->changing_pipeline = false;
-
-                    return GST_PAD_PROBE_REMOVE;
-                },
-                this, nullptr);
-        }
-    }
+            return GST_PAD_PROBE_REMOVE;
+        },
+        this, nullptr);
 }
 
 void PluginBase::disable() {
-    auto b = gst_bin_get_by_name(GST_BIN(plugin),
-                                 std::string(name + "_bin").c_str());
+    gst_pad_add_probe(gst_element_get_static_pad(identity_in, "src"),
+                      GST_PAD_PROBE_TYPE_BLOCK_DOWNSTREAM,
+                      [](auto pad, auto info, auto d) {
+                          auto l = static_cast<PluginBase*>(d);
 
-    if (b) {
-        bool changing;
+                          std::lock_guard<std::mutex> lock(mtx);
 
-        do {
-            changing = changing_pipeline.exchange(true);
+                          auto b = gst_bin_get_by_name(
+                              GST_BIN(l->plugin),
+                              std::string(l->name + "_bin").c_str());
 
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        } while (changing);
+                          if (b) {
+                              gst_element_unlink_many(l->identity_in, l->bin,
+                                                      l->identity_out, nullptr);
 
-        if (!changing) {
-            gst_pad_add_probe(
-                gst_element_get_static_pad(identity_in, "src"),
-                GST_PAD_PROBE_TYPE_IDLE,
-                [](auto pad, auto info, auto d) {
-                    auto l = static_cast<PluginBase*>(d);
+                              gst_bin_remove(GST_BIN(l->plugin), l->bin);
 
-                    gst_element_unlink_many(l->identity_in, l->bin,
-                                            l->identity_out, nullptr);
+                              gst_element_set_state(l->bin, GST_STATE_NULL);
 
-                    gst_bin_remove(GST_BIN(l->plugin), l->bin);
+                              gst_element_link(l->identity_in, l->identity_out);
 
-                    gst_element_set_state(l->bin, GST_STATE_NULL);
+                              gst_bin_sync_children_states(GST_BIN(l->plugin));
 
-                    gst_element_link(l->identity_in, l->identity_out);
+                              util::debug(l->log_tag + l->name + " disabled");
+                          }
 
-                    gst_bin_sync_children_states(GST_BIN(l->plugin));
-
-                    util::debug(l->log_tag + l->name + " disabled");
-
-                    l->changing_pipeline = false;
-
-                    return GST_PAD_PROBE_REMOVE;
-                },
-                this, nullptr);
-        }
-    }
+                          return GST_PAD_PROBE_REMOVE;
+                      },
+                      this, nullptr);
 }
