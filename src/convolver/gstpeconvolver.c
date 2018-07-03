2cead4b19158dd45fba39e23b7a9b9b980c3986b
wwmm <wellingtonwallace@gmail.com>
2018-07-03 19:43:45 -0300

    trying to add a convolver

--- src/convolver/gstpeconvolver.c
+++ src/convolver/gstpeconvolver.c
@@ -1,31 +1,5 @@
-/*
- * GStreamer
- * Copyright (C) 2005 Thomas Vander Stichele <thomas@apestaart.org>
- * Copyright (C) 2005 Ronald S. Bultje <rbultje@ronald.bitfreak.net>
- * Copyright (C) 2018 wallace <<user@hostname.org>>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- *
- * Alternatively, the contents of this file may be used under the
- * GNU Lesser General Public License Version 2.1 (the "LGPL"), in
- * which case the following provisions apply instead of the ones
- * mentioned above:
+/* GStreamer
+ * Copyright (C) 2018 FIXME <fixme@example.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -39,242 +13,214 @@
  *
  * You should have received a copy of the GNU Library General Public
  * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * Free Software Foundation, Inc., 51 Franklin Street, Suite 500,
+ * Boston, MA 02110-1335, USA.
  */
-
 /**
- * SECTION:element-peconvolver
+ * SECTION:element-gstpeconvolver
  *
- * FIXME:Describe peconvolver here.
+ * The peconvolver element does FIXME stuff.
  *
  * <refsect2>
  * <title>Example launch line</title>
  * |[
- * gst-launch -v autiotestsrc ! peconvolver ! pulsesink
+ * gst-launch-1.0 -v fakesrc ! peconvolver ! FIXME ! fakesink
  * ]|
+ * FIXME Describe what the pipeline does.
  * </refsect2>
  */
 
 #ifdef HAVE_CONFIG_H
-#include <config.h>
+#include "config.h"
 #endif
 
+#include <gst/audio/gstaudiofilter.h>
 #include <gst/gst.h>
-
 #include "gstpeconvolver.h"
 
-GST_DEBUG_CATEGORY_STATIC(gst_peconvolver_debug);
-#define GST_CAT_DEFAULT gst_peconvolver_debug
+GST_DEBUG_CATEGORY_STATIC(gst_peconvolver_debug_category);
+#define GST_CAT_DEFAULT gst_peconvolver_debug_category
 
-/* Filter signals and args */
-enum {
-    /* FILL ME */
-    LAST_SIGNAL
-};
-
-enum { PROP_0, PROP_SILENT };
-
-/* the capabilities of the inputs and outputs.
- *
- * describe the real formats here.
- */
-static GstStaticPadTemplate sink_factory =
-    GST_STATIC_PAD_TEMPLATE("sink",
-                            GST_PAD_SINK,
-                            GST_PAD_ALWAYS,
-                            GST_STATIC_CAPS("ANY"));
-
-static GstStaticPadTemplate src_factory =
-    GST_STATIC_PAD_TEMPLATE("src",
-                            GST_PAD_SRC,
-                            GST_PAD_ALWAYS,
-                            GST_STATIC_CAPS("ANY"));
-
-#define gst_peconvolver_parent_class parent_class
-G_DEFINE_TYPE(GstPEConvolver, gst_peconvolver, GST_TYPE_ELEMENT);
+/* prototypes */
 
 static void gst_peconvolver_set_property(GObject* object,
-                                         guint prop_id,
+                                         guint property_id,
                                          const GValue* value,
                                          GParamSpec* pspec);
 static void gst_peconvolver_get_property(GObject* object,
-                                         guint prop_id,
+                                         guint property_id,
                                          GValue* value,
                                          GParamSpec* pspec);
+static void gst_peconvolver_dispose(GObject* object);
+static void gst_peconvolver_finalize(GObject* object);
 
-static gboolean gst_peconvolver_sink_event(GstPad* pad,
-                                           GstObject* parent,
-                                           GstEvent* event);
-static GstFlowReturn gst_peconvolver_chain(GstPad* pad,
-                                           GstObject* parent,
-                                           GstBuffer* buf);
+static gboolean gst_peconvolver_setup(GstAudioFilter* filter,
+                                      const GstAudioInfo* info);
+static GstFlowReturn gst_peconvolver_transform(GstBaseTransform* trans,
+                                               GstBuffer* inbuf,
+                                               GstBuffer* outbuf);
+static GstFlowReturn gst_peconvolver_transform_ip(GstBaseTransform* trans,
+                                                  GstBuffer* buf);
 
-/* GObject vmethod implementations */
+enum { PROP_0 };
 
-/* initialize the peconvolver's class */
-static void gst_peconvolver_class_init(GstPEConvolverClass* klass) {
-    GObjectClass* gobject_class;
-    GstElementClass* gstelement_class;
+/* pad templates */
 
-    gobject_class = (GObjectClass*)klass;
-    gstelement_class = (GstElementClass*)klass;
+/* FIXME add/remove the formats that you want to support */
+static GstStaticPadTemplate gst_peconvolver_src_template =
+    GST_STATIC_PAD_TEMPLATE(
+        "src",
+        GST_PAD_SRC,
+        GST_PAD_ALWAYS,
+        GST_STATIC_CAPS("audio/x-raw,format=F32LE,rate=[1,max],"
+                        "channels=2,layout=interleaved"));
+
+/* FIXME add/remove the formats that you want to support */
+static GstStaticPadTemplate gst_peconvolver_sink_template =
+    GST_STATIC_PAD_TEMPLATE(
+        "sink",
+        GST_PAD_SINK,
+        GST_PAD_ALWAYS,
+        GST_STATIC_CAPS("audio/x-raw,format=F32LE,rate=[1,max],"
+                        "channels=2,layout=interleaved"));
+
+/* class initialization */
+
+G_DEFINE_TYPE_WITH_CODE(
+    GstPeconvolver,
+    gst_peconvolver,
+    GST_TYPE_AUDIO_FILTER,
+    GST_DEBUG_CATEGORY_INIT(gst_peconvolver_debug_category,
+                            "peconvolver",
+                            0,
+                            "debug category for peconvolver element"));
+
+static void gst_peconvolver_class_init(GstPeconvolverClass* klass) {
+    GObjectClass* gobject_class = G_OBJECT_CLASS(klass);
+    GstBaseTransformClass* base_transform_class =
+        GST_BASE_TRANSFORM_CLASS(klass);
+    GstAudioFilterClass* audio_filter_class = GST_AUDIO_FILTER_CLASS(klass);
+
+    /* Setting up pads and setting metadata should be moved to
+       base_class_init if you intend to subclass this class. */
+    gst_element_class_add_static_pad_template(GST_ELEMENT_CLASS(klass),
+                                              &gst_peconvolver_src_template);
+    gst_element_class_add_static_pad_template(GST_ELEMENT_CLASS(klass),
+                                              &gst_peconvolver_sink_template);
+
+    gst_element_class_set_static_metadata(
+        GST_ELEMENT_CLASS(klass), "PulseEffects Convolver", "Generic",
+        "PulseEffects Convolver", "Wellington <wellingtonwallace@gmail.com>");
 
     gobject_class->set_property = gst_peconvolver_set_property;
     gobject_class->get_property = gst_peconvolver_get_property;
-
-    g_object_class_install_property(
-        gobject_class, PROP_SILENT,
-        g_param_spec_boolean("silent", "Silent", "Produce verbose output ?",
-                             FALSE, G_PARAM_READWRITE));
-
-    gst_element_class_set_details_simple(
-        gstelement_class, "PEConvolver", "FIXME:Generic",
-        "FIXME:Generic Template Element", "wallace <<user@hostname.org>>");
-
-    gst_element_class_add_pad_template(
-        gstelement_class, gst_static_pad_template_get(&src_factory));
-    gst_element_class_add_pad_template(
-        gstelement_class, gst_static_pad_template_get(&sink_factory));
+    gobject_class->dispose = gst_peconvolver_dispose;
+    gobject_class->finalize = gst_peconvolver_finalize;
+    audio_filter_class->setup = GST_DEBUG_FUNCPTR(gst_peconvolver_setup);
+    base_transform_class->transform =
+        GST_DEBUG_FUNCPTR(gst_peconvolver_transform);
+    base_transform_class->transform_ip =
+        GST_DEBUG_FUNCPTR(gst_peconvolver_transform_ip);
 }
 
-/* initialize the new element
- * instantiate pads and add them to element
- * set pad calback functions
- * initialize instance structure
- */
-static void gst_peconvolver_init(GstPEConvolver* filter) {
-    filter->sinkpad = gst_pad_new_from_static_template(&sink_factory, "sink");
-    gst_pad_set_event_function(filter->sinkpad,
-                               GST_DEBUG_FUNCPTR(gst_peconvolver_sink_event));
-    gst_pad_set_chain_function(filter->sinkpad,
-                               GST_DEBUG_FUNCPTR(gst_peconvolver_chain));
-    GST_PAD_SET_PROXY_CAPS(filter->sinkpad);
-    gst_element_add_pad(GST_ELEMENT(filter), filter->sinkpad);
+static void gst_peconvolver_init(GstPeconvolver* peconvolver) {}
 
-    filter->srcpad = gst_pad_new_from_static_template(&src_factory, "src");
-    GST_PAD_SET_PROXY_CAPS(filter->srcpad);
-    gst_element_add_pad(GST_ELEMENT(filter), filter->srcpad);
+void gst_peconvolver_set_property(GObject* object,
+                                  guint property_id,
+                                  const GValue* value,
+                                  GParamSpec* pspec) {
+    GstPeconvolver* peconvolver = GST_PECONVOLVER(object);
 
-    filter->silent = FALSE;
-}
+    GST_DEBUG_OBJECT(peconvolver, "set_property");
 
-static void gst_peconvolver_set_property(GObject* object,
-                                         guint prop_id,
-                                         const GValue* value,
-                                         GParamSpec* pspec) {
-    GstPEConvolver* filter = GST_PECONVOLVER(object);
-
-    switch (prop_id) {
-        case PROP_SILENT:
-            filter->silent = g_value_get_boolean(value);
-            break;
+    switch (property_id) {
         default:
-            G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+            G_OBJECT_WARN_INVALID_PROPERTY_ID(object, property_id, pspec);
             break;
     }
 }
 
-static void gst_peconvolver_get_property(GObject* object,
-                                         guint prop_id,
-                                         GValue* value,
-                                         GParamSpec* pspec) {
-    GstPEConvolver* filter = GST_PECONVOLVER(object);
+void gst_peconvolver_get_property(GObject* object,
+                                  guint property_id,
+                                  GValue* value,
+                                  GParamSpec* pspec) {
+    GstPeconvolver* peconvolver = GST_PECONVOLVER(object);
 
-    switch (prop_id) {
-        case PROP_SILENT:
-            g_value_set_boolean(value, filter->silent);
-            break;
+    GST_DEBUG_OBJECT(peconvolver, "get_property");
+
+    switch (property_id) {
         default:
-            G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+            G_OBJECT_WARN_INVALID_PROPERTY_ID(object, property_id, pspec);
             break;
     }
 }
 
-/* GstElement vmethod implementations */
+void gst_peconvolver_dispose(GObject* object) {
+    GstPeconvolver* peconvolver = GST_PECONVOLVER(object);
 
-/* this function handles sink events */
-static gboolean gst_peconvolver_sink_event(GstPad* pad,
-                                           GstObject* parent,
-                                           GstEvent* event) {
-    GstPEConvolver* filter;
-    gboolean ret;
+    GST_DEBUG_OBJECT(peconvolver, "dispose");
 
-    filter = GST_PECONVOLVER(parent);
+    /* clean up as possible.  may be called multiple times */
 
-    GST_LOG_OBJECT(filter, "Received %s event: %" GST_PTR_FORMAT,
-                   GST_EVENT_TYPE_NAME(event), event);
-
-    switch (GST_EVENT_TYPE(event)) {
-        case GST_EVENT_CAPS: {
-            GstCaps* caps;
-
-            gst_event_parse_caps(event, &caps);
-            /* do something with the caps */
-
-            /* and forward */
-            ret = gst_pad_event_default(pad, parent, event);
-            break;
-        }
-        default:
-            ret = gst_pad_event_default(pad, parent, event);
-            break;
-    }
-    return ret;
+    G_OBJECT_CLASS(gst_peconvolver_parent_class)->dispose(object);
 }
 
-/* chain function
- * this function does the actual processing
- */
-static GstFlowReturn gst_peconvolver_chain(GstPad* pad,
-                                           GstObject* parent,
-                                           GstBuffer* buf) {
-    GstPEConvolver* filter;
+void gst_peconvolver_finalize(GObject* object) {
+    GstPeconvolver* peconvolver = GST_PECONVOLVER(object);
 
-    filter = GST_PECONVOLVER(parent);
+    GST_DEBUG_OBJECT(peconvolver, "finalize");
 
-    if (filter->silent == FALSE)
-        g_print("I'm plugged, therefore I'm in.\n");
+    /* clean up object here */
 
-    /* just push out the incoming buffer without touching it */
-    return gst_pad_push(filter->srcpad, buf);
+    G_OBJECT_CLASS(gst_peconvolver_parent_class)->finalize(object);
 }
 
-/* entry point to initialize the plug-in
- * initialize the plug-in itself
- * register the element factories and other features
- */
-static gboolean peconvolver_init(GstPlugin* peconvolver) {
-    /* debug category for fltering log messages
-     *
-     * exchange the string 'Template peconvolver' with your description
-     */
-    GST_DEBUG_CATEGORY_INIT(gst_peconvolver_debug, "peconvolver", 0,
-                            "Template peconvolver");
+static gboolean gst_peconvolver_setup(GstAudioFilter* filter,
+                                      const GstAudioInfo* info) {
+    GstPeconvolver* peconvolver = GST_PECONVOLVER(filter);
 
-    return gst_element_register(peconvolver, "peconvolver", GST_RANK_NONE,
+    GST_DEBUG_OBJECT(peconvolver, "setup");
+
+    return TRUE;
+}
+
+/* transform */
+static GstFlowReturn gst_peconvolver_transform(GstBaseTransform* trans,
+                                               GstBuffer* inbuf,
+                                               GstBuffer* outbuf) {
+    GstPeconvolver* peconvolver = GST_PECONVOLVER(trans);
+
+    GST_DEBUG_OBJECT(peconvolver, "transform");
+
+    return GST_FLOW_OK;
+}
+
+static GstFlowReturn gst_peconvolver_transform_ip(GstBaseTransform* trans,
+                                                  GstBuffer* buf) {
+    GstPeconvolver* peconvolver = GST_PECONVOLVER(trans);
+
+    GST_DEBUG_OBJECT(peconvolver, "transform_ip");
+
+    return GST_FLOW_OK;
+}
+
+static gboolean plugin_init(GstPlugin* plugin) {
+    /* FIXME Remember to set the rank if it's an element that is meant
+       to be autoplugged by decodebin. */
+    return gst_element_register(plugin, "peconvolver", GST_RANK_NONE,
                                 GST_TYPE_PECONVOLVER);
 }
 
-/* PACKAGE: this is usually set by autotools depending on some _INIT macro
- * in configure.ac and then written into and defined in config.h, but we can
- * just set it ourselves here in case someone doesn't use autotools to
- * compile this code. GST_PLUGIN_DEFINE needs PACKAGE to be defined.
- */
 #ifndef PACKAGE
-#define PACKAGE "peconvolver"
+#define PACKAGE "PulseEffects"
 #endif
 
-/* gstreamer looks for this structure to register peconvolvers
- *
- * exchange the string 'Template peconvolver' with your peconvolver description
- */
 GST_PLUGIN_DEFINE(GST_VERSION_MAJOR,
                   GST_VERSION_MINOR,
                   peconvolver,
                   "PulseEffects Convolver",
-                  peconvolver_init,
+                  plugin_init,
                   "0.1",
                   "LGPL",
-                  "PulseEffects",
+                  PACKAGE,
                   "https://github.com/wwmm/pulseeffects")
--- src/convolver/gstpeconvolver.h
+++ src/convolver/gstpeconvolver.h
@@ -1,31 +1,5 @@
-/*
- * GStreamer
- * Copyright (C) 2005 Thomas Vander Stichele <thomas@apestaart.org>
- * Copyright (C) 2005 Ronald S. Bultje <rbultje@ronald.bitfreak.net>
- * Copyright (C) 2018 wallace <<user@hostname.org>>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- *
- * Alternatively, the contents of this file may be used under the
- * GNU Lesser General Public License Version 2.1 (the "LGPL"), in
- * which case the following provisions apply instead of the ones
- * mentioned above:
+/* GStreamer
+ * Copyright (C) 2018 FIXME <fixme@example.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -39,47 +13,41 @@
  *
  * You should have received a copy of the GNU Library General Public
  * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
  */
 
-#ifndef __GST_PECONVOLVER_H__
-#define __GST_PECONVOLVER_H__
+#ifndef _GST_PECONVOLVER_H_
+#define _GST_PECONVOLVER_H_
 
-#include <gst/gst.h>
+#include <gst/audio/gstaudiofilter.h>
 
 G_BEGIN_DECLS
 
-/* #defines don't like whitespacey bits */
 #define GST_TYPE_PECONVOLVER (gst_peconvolver_get_type())
-#define GST_PECONVOLVER(obj)                                 \
-    (G_TYPE_CHECK_INSTANCE_CAST((obj), GST_TYPE_PECONVOLVER, \
-                                GstPEConvolver))
+#define GST_PECONVOLVER(obj) \
+    (G_TYPE_CHECK_INSTANCE_CAST((obj), GST_TYPE_PECONVOLVER, GstPeconvolver))
 #define GST_PECONVOLVER_CLASS(klass)                        \
     (G_TYPE_CHECK_CLASS_CAST((klass), GST_TYPE_PECONVOLVER, \
-                             GstPEConvolverClass))
+                             GstPeconvolverClass))
 #define GST_IS_PECONVOLVER(obj) \
     (G_TYPE_CHECK_INSTANCE_TYPE((obj), GST_TYPE_PECONVOLVER))
-#define GST_IS_PECONVOLVER_CLASS(klass) \
+#define GST_IS_PECONVOLVER_CLASS(obj) \
     (G_TYPE_CHECK_CLASS_TYPE((klass), GST_TYPE_PECONVOLVER))
 
-typedef struct _GstPEConvolver GstPEConvolver;
-typedef struct _GstPEConvolverClass GstPEConvolverClass;
+typedef struct _GstPeconvolver GstPeconvolver;
+typedef struct _GstPeconvolverClass GstPeconvolverClass;
 
-struct _GstPEConvolver {
-    GstElement element;
-
-    GstPad *sinkpad, *srcpad;
-
-    gboolean silent;
+struct _GstPeconvolver {
+    GstAudioFilter base_peconvolver;
 };
 
-struct _GstPEConvolverClass {
-    GstElementClass parent_class;
+struct _GstPeconvolverClass {
+    GstAudioFilterClass base_peconvolver_class;
 };
 
 GType gst_peconvolver_get_type(void);
 
 G_END_DECLS
 
-#endif /* __GST_PECONVOLVER_H__ */
+#endif
