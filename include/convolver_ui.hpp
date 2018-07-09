e1323d519e3f38af2f5281f4a98e5069660d248e
Wellington Wallace <wellingtonwallace@gmail.com>
2018-07-09 11:21:58 -0300

    improving the convolver

--- include/convolver_ui.hpp
+++ include/convolver_ui.hpp
@@ -1,7 +1,11 @@
 #ifndef CONVOLVER_UI_HPP
 #define CONVOLVER_UI_HPP
 
+#include <gtkmm/button.h>
 #include <gtkmm/grid.h>
+#include <gtkmm/listbox.h>
+#include <gtkmm/menubutton.h>
+#include <gtkmm/scrolledwindow.h>
 #include "plugin_ui_base.hpp"
 
 class ConvolverUi : public Gtk::Grid, public PluginUiBase {
@@ -15,6 +19,16 @@ class ConvolverUi : public Gtk::Grid, public PluginUiBase {
 
    private:
     Glib::RefPtr<Gtk::Adjustment> input_gain, output_gain;
+    Gtk::ListBox* irs_listbox;
+    Gtk::MenuButton* irs_menu_button;
+    Gtk::ScrolledWindow* irs_scrolled_window;
+    Gtk::Button* import_irs;
+
+    int on_listbox_sort(Gtk::ListBoxRow* row1, Gtk::ListBoxRow* row2);
+
+    void on_irs_menu_button_clicked();
+
+    void on_import_irs_clicked();
 };
 
 #endif
--- src/convolver_ui.cpp
+++ src/convolver_ui.cpp
@@ -1,3 +1,4 @@
+#include <glibmm/i18n.h>
 #include "convolver_ui.hpp"
 
 ConvolverUi::ConvolverUi(BaseObjectType* cobject,
@@ -8,10 +9,26 @@ ConvolverUi::ConvolverUi(BaseObjectType* cobject,
 
     // loading glade widgets
 
-    // builder->get_widget("mode", mode);
+    builder->get_widget("irs_listbox", irs_listbox);
+    builder->get_widget("irs_menu_button", irs_menu_button);
+    builder->get_widget("irs_scrolled_window", irs_scrolled_window);
+    builder->get_widget("import_irs", import_irs);
 
     get_object(builder, "input_gain", input_gain);
     get_object(builder, "output_gain", output_gain);
+
+    irs_menu_button->signal_clicked().connect(
+        sigc::mem_fun(*this, &ConvolverUi::on_irs_menu_button_clicked));
+
+    irs_listbox->set_sort_func(
+        sigc::mem_fun(*this, &ConvolverUi::on_listbox_sort));
+
+    irs_listbox->signal_row_activated().connect([&](auto row) {
+
+    });
+
+    import_irs->signal_clicked().connect(
+        sigc::mem_fun(*this, &ConvolverUi::on_import_irs_clicked));
 
     // gsettings bindings
 
@@ -33,6 +50,67 @@ ConvolverUi::~ConvolverUi() {
     settings->set_boolean("post-messages", false);
 
     util::debug(name + " ui destroyed");
+}
+
+int ConvolverUi::on_listbox_sort(Gtk::ListBoxRow* row1, Gtk::ListBoxRow* row2) {
+    auto name1 = row1->get_name();
+    auto name2 = row2->get_name();
+
+    std::vector<std::string> names = {name1, name2};
+
+    std::sort(names.begin(), names.end());
+
+    if (name1 == names[0]) {
+        return -1;
+    } else if (name2 == names[0]) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void ConvolverUi::on_irs_menu_button_clicked() {
+    int height = 0.7 * get_allocated_height();
+
+    irs_scrolled_window->set_max_content_height(height);
+
+    // populate_irs_listbox();
+}
+
+void ConvolverUi::on_import_irs_clicked() {
+    // gtkmm 3.22 does not have FileChooseNative so we have to use C api :-(
+
+    GtkFileChooserNative* native;
+    GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_OPEN;
+    gint res;
+
+    native = gtk_file_chooser_native_new(_("Import Presets"),
+                                         (GtkWindow*)this->gobj(), action,
+                                         _("Open"), _("Cancel"));
+
+    res = gtk_native_dialog_run(GTK_NATIVE_DIALOG(native));
+
+    if (res == GTK_RESPONSE_ACCEPT) {
+        GtkFileChooser* chooser = GTK_FILE_CHOOSER(native);
+
+        auto file_list = gtk_file_chooser_get_filenames(chooser);
+
+        g_slist_foreach(file_list,
+                        [](auto data, auto user_data) {
+                            // auto cui = static_cast<ConvolverUi*>(user_data);
+
+                            // auto file_path = static_cast<char*>(data);
+
+                            // aui->app->irs_manager->import(file_path);
+                        },
+                        this);
+
+        g_slist_free(file_list);
+    }
+
+    g_object_unref(native);
+
+    // populate_irs_listbox();
 }
 
 void ConvolverUi::reset() {
