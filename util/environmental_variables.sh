6c6fcf9ab16188b1dcc0855e0ffa4ff1c273480f
Wellington Wallace <wellingtonwallace@gmail.com>
2018-11-30 21:15:43 -0200

    added a environmental variable to the util script

--- util/environmental_variables.sh
+++ util/environmental_variables.sh
@@ -3,3 +3,4 @@
 export GSETTINGS_SCHEMA_DIR=../../data/schemas/
 export GST_PLUGIN_PATH_1_0=convolver:crystalizer:autogain:adapter
 export G_MESSAGES_DEBUG=pulseeffects
+export GST_DEBUG_FILE=/tmp/gst_pe_log.txt
