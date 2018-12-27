b85712f2a747f53202d7e8200e123f09d2c0e000
Wellington Wallace <wellingtonwallace@gmail.com>
2018-12-27 14:42:09 -0200

    avoiding unnecessaries writes to the gsettings database when loading a preset

--- src/equalizer_preset.cpp
+++ src/equalizer_preset.cpp
@@ -54,21 +54,6 @@ void EqualizerPreset::load(boost::property_tree::ptree& root,
   update_key<double>(root, settings, "output-gain",
                      section + ".equalizer.output-gain");
 
-  // settings->set_boolean("state",
-  //                       root.get<bool>(section + ".equalizer.state",
-  //                                      get_default<bool>(settings,
-  //                                      "state")));
-
-  // settings->set_double(
-  //     "input-gain",
-  //     root.get<double>(section + ".equalizer.input-gain",
-  //                      get_default<double>(settings, "input-gain")));
-  //
-  // settings->set_double(
-  //     "output-gain",
-  //     root.get<double>(section + ".equalizer.output-gain",
-  //                      get_default<double>(settings, "output-gain")));
-
   int nbands = settings->get_int("num-bands");
 
   for (int n = 0; n < nbands; n++) {
@@ -87,38 +72,6 @@ void EqualizerPreset::load(boost::property_tree::ptree& root,
     update_string_key(
         root, settings, std::string("band" + std::to_string(n) + "-type"),
         section + ".equalizer.band" + std::to_string(n) + ".type");
-
-    // settings->set_double(
-    //     std::string("band" + std::to_string(n) + "-gain"),
-    //     root.get<double>(
-    //         section + ".equalizer.band" + std::to_string(n) + ".gain",
-    //         get_default<double>(
-    //             settings, std::string("band" + std::to_string(n) +
-    //             "-gain"))));
-
-    // settings->set_double(
-    //     std::string("band" + std::to_string(n) + "-frequency"),
-    //     root.get<double>(
-    //         section + ".equalizer.band" + std::to_string(n) + ".frequency",
-    //         get_default<double>(
-    //             settings,
-    //             std::string("band" + std::to_string(n) + "-frequency"))));
-
-    // settings->set_double(
-    //     std::string("band" + std::to_string(n) + "-width"),
-    //     root.get<double>(
-    //         section + ".equalizer.band" + std::to_string(n) + ".width",
-    //         get_default<double>(
-    //             settings, std::string("band" + std::to_string(n) +
-    //             "-width"))));
-
-    // settings->set_string(
-    //     std::string("band" + std::to_string(n) + "-type"),
-    //     root.get<std::string>(
-    //         section + ".equalizer.band" + std::to_string(n) + ".type",
-    //         get_default<std::string>(
-    //             settings, std::string("band" + std::to_string(n) +
-    //             "-type"))));
   }
 }
 
