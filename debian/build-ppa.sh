b4408043600253a31627e45cccde731e91f28a18
Mikhail Novosyolov <mikhailnov@dumalogiya.ru>
2018-12-31 08:54:04 +0300

    Workaround Launchpad not accepting *.git and dpkg-* trying to use existing tarballs (which are e.g. downloaded by uscan in util/autobuild.sh)

--- debian/build-ppa.sh
+++ debian/build-ppa.sh
@@ -15,21 +15,42 @@ debian/rules clean
 dir0="$(pwd)"
 old_header=$(head -1 ./debian/changelog)
 old_format_source=$(cat ./debian/source/format)
+tmp_suffix="buildPPA_$(date +%s)_tmp"
 
 for i in bionic cosmic disco
 do
 	old_version="$(cat ./debian/changelog | head -n 1 | awk -F "(" '{print $2}' | awk -F ")" '{print $1}')"
 	new_version="${old_version}~${i}1"
+	real_version="$(echo "$old_version" | cut -d'-' -f1)"
 	sed -i -re "s/${old_version}/${new_version}/g" ./debian/changelog
 	sed -i -re "1s/unstable/$i/" ./debian/changelog
 	if grep -q '(git)' ./debian/source/format; then
 		GIT=1
+		old_source_format="$(cat ./debian/source/format)"
+		# TODO: Launchpad PPA does not accept in '3.0 (git)' for some reasons
+		rm -fv ./debian/source/format
 		git add .
+		# dpkg-buildpackage also tries to diff current git vs tarball if it finds it in ../
+		while read line
+		do
+			# TODO: check if target file already exists
+			mv -v "$line" "${line}.${tmp_suffix}"
+		done < <(find ../ -maxdepth 1 -name "${pkg_name}_${real_version}.orig.tar*")
 	fi
 	# -I to exclude .git; -d to allow building .changes file without build dependencies installed
-	dpkg-buildpackage -S -sa -d
+	dpkg-buildpackage -I -S -sa -d
 	sed  -i -re "1s/.*/${old_header}/" ./debian/changelog
 	[ "$GIT" = 1 ] && git reset
+	[ -n "$old_source_format" ] && echo "$old_source_format" > ./debian/source/format
+	
+	# dpkg-buildpackage also tries to diff current git vs tarball if it finds it in ../
+	while read line
+	do
+		new_name="$(echo "$line" | sed -e "s/.${tmp_suffix}$//g")"
+		# TODO: check if target file already exists
+		mv -v "$line" "$new_name"
+	done < <(find ../ -maxdepth 1 -name "*.${tmp_suffix}")
+		
 	cd ..
 	
 	# change PPA names to yours, you may leave only one PPA; I upload hw-probe to 2 different PPAs at the same time
