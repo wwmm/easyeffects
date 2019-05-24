475dc6e7be9e4427fc4382f80786ad67d1e0564e
Wellington Wallace <wellingtonwallace@gmail.com>
2019-05-24 20:24:05 -0300

    improved circleci tests

---
+++ .circleci/disable_all_plugins.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# general settings
+
+gsettings set com.github.wwmm.pulseeffects.enable-all-sourceoutputs false
+
+# effects for sink inputs
+
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.autogain state false
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.bassenhancer state false
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.compressor state false
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.convolver state false
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.crossfeed state false
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.crystalizer state false
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.deesser state false
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.delay state false
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.equalizer state false
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.exciter state false
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.filter state false
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.gate state false
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.limiter state false
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.loudness state false
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.maximizer state false
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.multibandcompressor state false
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.multibandgate state false
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.pitch state false
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.reverb state false
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.stereotools state false
+
+# effects for source outputs
+
+gsettings set com.github.wwmm.pulseeffects.sourceoutputs.compressor state false
+gsettings set com.github.wwmm.pulseeffects.sourceoutputs.deesser state false
+gsettings set com.github.wwmm.pulseeffects.sourceoutputs.equalizer state false
+gsettings set com.github.wwmm.pulseeffects.sourceoutputs.filter state false
+gsettings set com.github.wwmm.pulseeffects.sourceoutputs.gate state false
+gsettings set com.github.wwmm.pulseeffects.sourceoutputs.limiter state false
+gsettings set com.github.wwmm.pulseeffects.sourceoutputs.multibandcompressor state false
+gsettings set com.github.wwmm.pulseeffects.sourceoutputs.multibandgate state false
+gsettings set com.github.wwmm.pulseeffects.sourceoutputs.pitch state false
+gsettings set com.github.wwmm.pulseeffects.sourceoutputs.reverb state false
+gsettings set com.github.wwmm.pulseeffects.sourceoutputs.webrtc state false
---
+++ .circleci/enable_all_plugins.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# general settings
+
+gsettings set com.github.wwmm.pulseeffects.enable-all-sourceoutputs true
+
+# effects for sink inputs
+
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.autogain state true
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.bassenhancer state true
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.compressor state true
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.convolver state true
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.crossfeed state true
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.crystalizer state true
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.deesser state true
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.delay state true
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.equalizer state true
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.exciter state true
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.filter state true
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.gate state true
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.limiter state true
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.loudness state true
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.maximizer state true
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.multibandcompressor state true
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.multibandgate state true
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.pitch state true
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.reverb state true
+gsettings set com.github.wwmm.pulseeffects.sinkinputs.stereotools state true
+
+# effects for source outputs
+
+gsettings set com.github.wwmm.pulseeffects.sourceoutputs.compressor state true
+gsettings set com.github.wwmm.pulseeffects.sourceoutputs.deesser state true
+gsettings set com.github.wwmm.pulseeffects.sourceoutputs.equalizer state true
+gsettings set com.github.wwmm.pulseeffects.sourceoutputs.filter state true
+gsettings set com.github.wwmm.pulseeffects.sourceoutputs.gate state true
+gsettings set com.github.wwmm.pulseeffects.sourceoutputs.limiter state true
+gsettings set com.github.wwmm.pulseeffects.sourceoutputs.multibandcompressor state true
+gsettings set com.github.wwmm.pulseeffects.sourceoutputs.multibandgate state true
+gsettings set com.github.wwmm.pulseeffects.sourceoutputs.pitch state true
+gsettings set com.github.wwmm.pulseeffects.sourceoutputs.reverb state true
+gsettings set com.github.wwmm.pulseeffects.sourceoutputs.webrtc state true
