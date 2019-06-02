6ec2d7ee0d323e77a7c04e1353ac3fd79476c562
Wellington Wallace <wellingtonwallace@gmail.com>
2019-06-02 11:13:27 -0300

    using 25 ms as default release value in the maximizer

--- src/pulse_manager.cpp
+++ src/pulse_manager.cpp
@@ -332,11 +332,11 @@ void PulseManager::update_server_info(const pa_server_info* info) {
   server_info.format = pa_sample_format_to_string(info->sample_spec.format);
   server_info.rate = info->sample_spec.rate;
   server_info.channels = info->sample_spec.channels;
+
   if (pa_channel_map_to_pretty_name(&info->channel_map) != nullptr) {
-      server_info.channel_map = pa_channel_map_to_pretty_name(&info->channel_map);
-  }
-  else {
-      server_info.channel_map = "unknown";
+    server_info.channel_map = pa_channel_map_to_pretty_name(&info->channel_map);
+  } else {
+    server_info.channel_map = "unknown";
   }
 }
 
