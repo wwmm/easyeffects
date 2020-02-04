6762a8e467a2c97ecb0e88bcb69166546694fac0
Wellington Wallace <wellingtonwallace@gmail.com>
2020-02-04 19:22:52 -0300

    avoid implicit conversions

--- src/multiband_gate_ui.cpp
+++ src/multiband_gate_ui.cpp
@@ -11,7 +11,7 @@ auto detection_enum_to_int(GValue* value, GVariant* variant, gpointer user_data)
     g_value_set_int(value, 1);
   }
 
-  return true;
+  return 1;
 }
 
 auto int_to_detection_enum(const GValue* value, const GVariantType* expected_type, gpointer user_data) -> GVariant* {
@@ -33,7 +33,7 @@ auto mode_enum_to_int(GValue* value, GVariant* variant, gpointer user_data) -> g
     g_value_set_int(value, 1);
   }
 
-  return true;
+  return 1;
 }
 
 auto int_to_mode_enum(const GValue* value, const GVariantType* expected_type, gpointer user_data) -> GVariant* {
@@ -192,47 +192,47 @@ MultibandGateUi::~MultibandGateUi() {
 void MultibandGateUi::on_new_output0(double value) {
   output0->set_value(value);
 
-  output0_label->set_text(level_to_str(util::linear_to_db(value), 0));
+  output0_label->set_text(level_to_str(util::linear_to_db(static_cast<float>(value)), 0));
 }
 
 void MultibandGateUi::on_new_output1(double value) {
   output1->set_value(value);
 
-  output1_label->set_text(level_to_str(util::linear_to_db(value), 0));
+  output1_label->set_text(level_to_str(util::linear_to_db(static_cast<float>(value)), 0));
 }
 
 void MultibandGateUi::on_new_output2(double value) {
   output2->set_value(value);
 
-  output2_label->set_text(level_to_str(util::linear_to_db(value), 0));
+  output2_label->set_text(level_to_str(util::linear_to_db(static_cast<float>(value)), 0));
 }
 
 void MultibandGateUi::on_new_output3(double value) {
   output3->set_value(value);
 
-  output3_label->set_text(level_to_str(util::linear_to_db(value), 0));
+  output3_label->set_text(level_to_str(util::linear_to_db(static_cast<float>(value)), 0));
 }
 
 void MultibandGateUi::on_new_gating0(double value) {
   gating0->set_value(1 - value);
 
-  gating0_label->set_text(level_to_str(util::linear_to_db(value), 0));
+  gating0_label->set_text(level_to_str(util::linear_to_db(static_cast<float>(value)), 0));
 }
 
 void MultibandGateUi::on_new_gating1(double value) {
   gating1->set_value(1 - value);
 
-  gating1_label->set_text(level_to_str(util::linear_to_db(value), 0));
+  gating1_label->set_text(level_to_str(util::linear_to_db(static_cast<float>(value)), 0));
 }
 
 void MultibandGateUi::on_new_gating2(double value) {
   gating2->set_value(1 - value);
 
-  gating2_label->set_text(level_to_str(util::linear_to_db(value), 0));
+  gating2_label->set_text(level_to_str(util::linear_to_db(static_cast<float>(value)), 0));
 }
 
 void MultibandGateUi::on_new_gating3(double value) {
   gating3->set_value(1 - value);
 
-  gating3_label->set_text(level_to_str(util::linear_to_db(value), 0));
+  gating3_label->set_text(level_to_str(util::linear_to_db(static_cast<float>(value)), 0));
 }
