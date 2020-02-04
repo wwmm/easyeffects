c9df3f1d50f207912a14f448e7a74696b5635644
Wellington Wallace <wellingtonwallace@gmail.com>
2020-02-04 12:58:18 -0300

    disabling copy and move constructors where a custom destructor was defined

--- include/loudness_ui.hpp
+++ include/loudness_ui.hpp
@@ -7,10 +7,12 @@
 
 class LoudnessUi : public Gtk::Grid, public PluginUiBase {
  public:
-  LoudnessUi(BaseObjectType* cobject,
-             const Glib::RefPtr<Gtk::Builder>& builder,
-             const std::string& settings_name);
-  virtual ~LoudnessUi();
+  LoudnessUi(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& builder, const std::string& settings_name);
+  LoudnessUi(const LoudnessUi&) = delete;
+  auto operator=(const LoudnessUi&) -> LoudnessUi& = delete;
+  LoudnessUi(const LoudnessUi&&) = delete;
+  auto operator=(const LoudnessUi &&) -> LoudnessUi& = delete;
+  ~LoudnessUi() override;
 
  private:
   Glib::RefPtr<Gtk::Adjustment> loudness, output, link;
