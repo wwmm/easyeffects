a72bac8caa4447fddf0563fac582c78c4d93e4a9
Wellington Wallace <wellingtonwallace@gmail.com>
2020-02-03 21:27:32 -0300

    disabling copy and move constructors where a custom destructor was defined

--- include/delay_ui.hpp
+++ include/delay_ui.hpp
@@ -7,7 +7,11 @@
 class DelayUi : public Gtk::Grid, public PluginUiBase {
  public:
   DelayUi(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& builder, const std::string& settings_name);
-  virtual ~DelayUi();
+  DelayUi(const DelayUi&) = delete;
+  auto operator=(const DelayUi&) -> DelayUi& = delete;
+  DelayUi(const DelayUi&&) = delete;
+  auto operator=(const DelayUi &&) -> DelayUi& = delete;
+  ~DelayUi() override;
 
  private:
   Glib::RefPtr<Gtk::Adjustment> input_gain, output_gain, time_l, time_r;
