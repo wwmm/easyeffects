18df162b9301824df25b29acab4dff192a832bed
Mikhail Novosyolov <m.novosyolov@rosalinux.ru>
2020-03-23 11:42:49 +0300

    debian: drop odd PPA

--- debian/build-ppa.sh
+++ debian/build-ppa.sh
@@ -53,8 +53,8 @@ do
 
 	cd ..
 
-	# change PPA names to yours, you may leave only one PPA; I upload hw-probe to 2 different PPAs at the same time
-	for ppa_name in ppa:mikhailnov/pulseeffects ppa:mikhailnov/utils ppa:mikhailnov/desktop1-dev
+	# change PPA names to yours, you may leave only one PPA; I upload pulseeffects to 2 different PPAs at the same time
+	for ppa_name in ppa:mikhailnov/pulseeffects ppa:mikhailnov/utils
 	do
 		# example file name: pulseeffects_4.4.3-1~bionic1_source.changes
 		if [ -f "${pkg_name}_${new_version}_source.changes" ]
