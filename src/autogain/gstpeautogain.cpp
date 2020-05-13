03ea697a50ba267949ea643123bb908fee38afa4
Mlocik97 <Mlocik97@gmail.com>
2020-05-13 11:19:18 +0200

    Update gstpeautogain.cpp

--- src/autogain/gstpeautogain.cpp
+++ src/autogain/gstpeautogain.cpp
@@ -165,7 +165,7 @@ static void gst_peautogain_class_init(GstPeautogainClass* klass) {
 
   g_object_class_install_property(
       gobject_class, PROP_SV,
-      g_param_spec_float("staticv", "Static Value", "Static Value", -100.0f, 0.0f, -12,0f,
+      g_param_spec_float("staticv", "Static Value", "Static Value", -100.0f, 0.0f, -12.0f,
                            static_cast<GParamFlags>(G_PARAM_READABLE | G_PARAM_STATIC_STRINGS)));
 
   g_object_class_install_property(
