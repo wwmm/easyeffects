6edc224b7ea63bccb27f6a613edddb43c33adf8b
Wellington Wallace <wellingtonwallace@gmail.com>
2020-06-17 23:18:49 -0300

    including headers that are necessary on FreeBSD

--- src/filter.cpp
+++ src/filter.cpp
@@ -1,5 +1,6 @@
 #include "filter.hpp"
 #include <glibmm/main.h>
+#include <array>
 #include "util.hpp"
 
 namespace {
@@ -11,8 +12,8 @@ void on_post_messages_changed(GSettings* settings, gchar* key, Filter* l) {
     if (!l->input_level_connection.connected()) {
       l->input_level_connection = Glib::signal_timeout().connect(
           [l]() {
-            float inL;
-            float inR;
+            float inL = 0.0;
+            float inR = 0.0;
 
             g_object_get(l->filter, "meter-inL", &inL, nullptr);
             g_object_get(l->filter, "meter-inR", &inR, nullptr);
@@ -29,8 +30,8 @@ void on_post_messages_changed(GSettings* settings, gchar* key, Filter* l) {
     if (!l->output_level_connection.connected()) {
       l->output_level_connection = Glib::signal_timeout().connect(
           [l]() {
-            float outL;
-            float outR;
+            float outL = 0.0;
+            float outR = 0.0;
 
             g_object_get(l->filter, "meter-outL", &outL, nullptr);
             g_object_get(l->filter, "meter-outR", &outR, nullptr);
@@ -55,15 +56,15 @@ Filter::Filter(const std::string& tag, const std::string& schema) : PluginBase(t
   filter = gst_element_factory_make("calf-sourceforge-net-plugins-Filter", "filter");
 
   if (is_installed(filter)) {
-    auto audioconvert_in = gst_element_factory_make("audioconvert", "filter_audioconvert_in");
-    auto audioconvert_out = gst_element_factory_make("audioconvert", "filter_audioconvert_out");
+    auto* audioconvert_in = gst_element_factory_make("audioconvert", "filter_audioconvert_in");
+    auto* audioconvert_out = gst_element_factory_make("audioconvert", "filter_audioconvert_out");
 
     gst_bin_add_many(GST_BIN(bin), audioconvert_in, filter, audioconvert_out, nullptr);
 
     gst_element_link_many(audioconvert_in, filter, audioconvert_out, nullptr);
 
-    auto pad_sink = gst_element_get_static_pad(audioconvert_in, "sink");
-    auto pad_src = gst_element_get_static_pad(audioconvert_out, "src");
+    auto* pad_sink = gst_element_get_static_pad(audioconvert_in, "sink");
+    auto* pad_src = gst_element_get_static_pad(audioconvert_out, "src");
 
     gst_element_add_pad(bin, gst_ghost_pad_new("sink", pad_sink));
     gst_element_add_pad(bin, gst_ghost_pad_new("src", pad_src));
