41b84d21aa73f2527090657fe9b1b00c208d0182
wwmm <wellingtonwallace@gmail.com>
2021-07-03 13:23:31 -0300

    trying to write a signal generator

--- src/test_signals.cpp
+++ src/test_signals.cpp
@@ -61,7 +61,7 @@ TestSignals::TestSignals(PipeManager* pipe_manager) : pm(pipe_manager) {
   pw_properties_set(props_filter, PW_KEY_MEDIA_TYPE, "Audio");
   pw_properties_set(props_filter, PW_KEY_MEDIA_CATEGORY, "Filter");
   pw_properties_set(props_filter, PW_KEY_MEDIA_ROLE, "DSP");
-  pw_properties_set(props_filter, PW_KEY_MEDIA_CLASS, "Stream/Output/Audio");
+  //   pw_properties_set(props_filter, PW_KEY_MEDIA_CLASS, "Stream/Output/Audio");
 
   filter = pw_filter_new(pm->core, filter_name, props_filter);
 
