28692bd1911832a09947db37cff67eca663553a3
Vincent Chernin <38842733+vchernin@users.noreply.github.com>
2023-03-17 18:31:57 -0700

    chore: Rework clang-tidy file This should contain the absolute bare minimum rules disabled given we enable basically all llvm rules except for coding conventions
    Note we treat clang-diagnostic-* as warnings not errors
    Since otherwise clang-tidy fails even with clang 16, we want to ignore these warnings for now

---
+++ util/clang-tidy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+# write a custom script for ease of use, meson clang-tidy target can't do fixes, and also has somewhat broken threading
+
+if ! meson introspect --projectinfo &> /dev/null; then
+    echo "Error, meson introspect command failed, make sure you are in a meson build directory"
+    exit 1
+fi
+
+if [[ "$PWD" == "" ]]; then
+    echo "Error, \$PWD is empty"
+    exit 1
+fi
+
+# save current dir and get repo dir
+# we need an absolute path without any .. in them otherwise these various layers of scripts get confused
+BUILD_DIR="$PWD"
+
+cd ..
+REPO_DIR="$PWD"
+
+# back to build dir
+cd "$BUILD_DIR"
+
+# this depends on a hack in the python wrapper script to add the header files even if they are not in the compilation database, otherwise header files are not checked at all
+# it is fine to use -fix everywhere, since it will still exit with error even if errors were all automatically fixed.
+# however we do not use -fix since it is safer to just manually fix things
+python3 ../util/run-clang-tidy.py -p . -config-file=../.clang-tidy "$REPO_DIR"/src/*.cpp "$REPO_DIR"/include/*.hpp
