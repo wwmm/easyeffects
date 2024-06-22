c1b678a11846aa6eb9a3392a5332e8c1c5dab713
wwmm <wellingtonwallace@gmail.com>
2024-06-22 11:24:02 -0300

    pipe_manager: improving the logic used to avoid EE messing with custom routing

--- src/pipe_manager.cpp
+++ src/pipe_manager.cpp
@@ -321,9 +321,10 @@ void on_node_info(void* object, const struct pw_node_info* info) {
 
       util::str_to_num(target_object, serial);
 
-      if (target_object != pm->input_device.name) {
+      if (target_object != pm->input_device.name || target_object != pm->ee_source_node.name) {
         ignore_input_stream = true;
-      } else if (serial != SPA_ID_INVALID && serial != pm->input_device.serial) {
+      } else if (serial != SPA_ID_INVALID &&
+                 (serial != pm->input_device.serial && serial != pm->ee_source_node.serial)) {
         ignore_input_stream = true;
       }
 
@@ -348,9 +349,10 @@ void on_node_info(void* object, const struct pw_node_info* info) {
 
       util::str_to_num(target_object, serial);
 
-      if (target_object != pm->output_device.name) {
+      if (target_object != pm->output_device.name || target_object != pm->ee_sink_node.name) {
         ignore_output_stream = true;
-      } else if (serial != SPA_ID_INVALID && serial != pm->output_device.serial) {
+      } else if (serial != SPA_ID_INVALID &&
+                 (serial != pm->output_device.serial && serial != pm->ee_sink_node.serial)) {
         ignore_output_stream = true;
       }
 
