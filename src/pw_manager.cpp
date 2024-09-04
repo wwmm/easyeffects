17f309b4c65e7516ecf251c89194a3f0267c854c
wwmm <wellingtonwallace@gmail.com>
2024-09-04 20:59:17 -0300

    pipe_manager: do not ignore target objects set through numbers  when deciding if we should ignore a stream or not

--- src/pw_manager.cpp
+++ src/pw_manager.cpp
@@ -296,8 +296,9 @@ void on_node_info(void* object, const struct pw_node_info* info) {
 
       if (target_object != pm->input_device.name && target_object != pm->ee_source_node.name) {
         ignore_input_stream = true;
-      } else if (serial != SPA_ID_INVALID &&
-                 (serial != pm->input_device.serial && serial != pm->ee_source_node.serial)) {
+      }
+
+      if (serial != SPA_ID_INVALID && (serial != pm->input_device.serial && serial != pm->ee_source_node.serial)) {
         ignore_input_stream = true;
       }
 
@@ -324,8 +325,9 @@ void on_node_info(void* object, const struct pw_node_info* info) {
 
       if (target_object != pm->output_device.name && target_object != pm->ee_sink_node.name) {
         ignore_output_stream = true;
-      } else if (serial != SPA_ID_INVALID &&
-                 (serial != pm->output_device.serial && serial != pm->ee_sink_node.serial)) {
+      }
+
+      if (serial != SPA_ID_INVALID && (serial != pm->output_device.serial && serial != pm->ee_sink_node.serial)) {
         ignore_output_stream = true;
       }
 
