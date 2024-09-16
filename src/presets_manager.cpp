fbe0a724c1405cee624802f381476cf003dfcfa5
Wellington Wallace <wellingtonwallace@gmail.com>
2024-09-16 19:24:13 -0300

    Using Boost >= 1.86

--- src/presets_manager.cpp
+++ src/presets_manager.cpp
@@ -472,7 +472,7 @@ void PresetsManager::import(PresetType preset_type, const std::string& file_path
 
       out_path = user_dir / p.filename();
 
-      boost::filesystem::copy_file(p, out_path, std::filesystem::copy_options::overwrite_existing);
+      boost::filesystem::copy_file(p, out_path, boost::filesystem::copy_options::overwrite_existing);
 
       util::debug(log_tag + "imported preset to: " + out_path.string());
     }
