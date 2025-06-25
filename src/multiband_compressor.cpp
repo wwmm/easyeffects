1822435759e3df753f721fa2cec95fdbde835e56
Digitalone1 <kurmikon@yahoo.com>
2025-06-25 16:22:13 +0200

    implement the mb compressor plugin class

---
+++ src/multiband_compressor.cpp
@@ -0,0 +1,118 @@
+/*
+ *  Copyright © 2017-2025 Wellington Wallace
+ *
+ *  This file is part of Easy Effects.
+ *
+ *  Easy Effects is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Easy Effects is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Easy Effects. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "multiband_compressor.hpp"
+#include <qtypes.h>
+#include <algorithm>
+#include <memory>
+#include <span>
+#include <string>
+#include "db_manager.hpp"
+#include "easyeffects_db_multiband_compressor.h"
+#include "lv2_macros.hpp"
+#include "lv2_wrapper.hpp"
+#include "pipeline_type.hpp"
+#include "plugin_base.hpp"
+#include "pw_manager.hpp"
+#include "pw_objects.hpp"
+#include "spa/utils/defs.h"
+#include "tags_multiband_compressor.hpp"
+#include "tags_plugin_name.hpp"
+#include "util.hpp"
+
+MultibandCompressor::MultibandCompressor(const std::string& tag,
+                                         pw::Manager* pipe_manager,
+                                         PipelineType pipe_type,
+                                         QString instance_id)
+    : PluginBase(tag,
+                 tags::plugin_name::BaseName::multiband_compressor,
+                 tags::plugin_package::Package::lsp,
+                 instance_id,
+                 pipe_manager,
+                 pipe_type,
+                 true),
+      settings(db::Manager::self().get_plugin_db<db::MultibandCompressor>(
+          pipe_type,
+          tags::plugin_name::BaseName::multiband_compressor + "#" + instance_id)) {
+  const auto lv2_plugin_uri = "http://lsp-plug.in/plugins/lv2/sc_mb_compressor_stereo";
+
+  lv2_wrapper = std::make_unique<lv2::Lv2Wrapper>(lv2_plugin_uri);
+
+  package_installed = lv2_wrapper->found_plugin;
+
+  if (!package_installed) {
+    util::debug(log_tag + lv2_plugin_uri + " is not installed");
+  }
+
+  init_common_controls<db::MultibandCompressor>(settings);
+
+  // specific plugin controls
+
+  // TODO: set sidechain controls
+  // ...
+
+  BIND_LV2_PORT("mode", compressorMode, setCompressorMode, db::MultibandCompressor::compressorModeChanged);
+  BIND_LV2_PORT("envb", envelopeBoost, setEnvelopeBoost, db::MultibandCompressor::envelopeBoostChanged);
+  BIND_LV2_PORT("ssplit", stereoSplit, setStereoSplit, db::MultibandCompressor::stereoSplitChanged);
+
+  // dB controls with -inf mode.
+  BIND_LV2_PORT_DB("cdr", g_dry, setDry, db::MultibandCompressor::dryChanged, true);
+  BIND_LV2_PORT_DB("cwt", g_wet, setWet, db::MultibandCompressor::wetChanged, true);
+
+  // TODO: set band parameters
+  // ...
+}
+
+MultibandCompressor::~MultibandCompressor() {
+  if (connected_to_pw) {
+    disconnect_from_pw();
+  }
+
+  settings->disconnect();
+
+  util::debug(log_tag + name.toStdString() + " destroyed");
+}
+
+void MultibandCompressor::reset() {
+  settings->setDefaults();
+}
+
+void MultibandCompressor::setup() {
+  if (!lv2_wrapper->found_plugin) {
+    return;
+  }
+
+  lv2_wrapper->set_n_samples(n_samples);
+
+  if (lv2_wrapper->get_rate() != rate) {
+    lv2_wrapper->create_instance(rate);
+  }
+}
+
+void MultibandCompressor::process([[maybe_unused]] std::span<float>& left_in,
+                                  [[maybe_unused]] std::span<float>& right_in,
+                                  [[maybe_unused]] std::span<float>& left_out,
+                                  [[maybe_unused]] std::span<float>& right_out) {}
+
+void MultibandCompressor::process(std::span<float>& left_in,
+                                  std::span<float>& right_in,
+                                  std::span<float>& left_out,
+                                  std::span<float>& right_out,
+                                  std::span<float>& probe_left,
+                                  std::span<float>& probe_right) {}
