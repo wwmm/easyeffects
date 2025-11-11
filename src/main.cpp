dc57a013f1cac6a35d1a4f2e3fbfa75df5207a4f
David Hess <76759448+Lunix-420@users.noreply.github.com>
2025-11-11 06:00:10 +0100

    Update application style and icon theme settings
    Set default style and icon theme for application.

--- src/main.cpp
+++ src/main.cpp
@@ -38,6 +38,7 @@
 #include <QLocalServer>
 #include <QLoggingCategory>
 #include <QProcessEnvironment>
+#include <QQuickStyle>
 #include <QQuickWindow>
 #include <QSystemTrayIcon>
 #include <csignal>
@@ -299,10 +300,12 @@ int main(int argc, char* argv[]) {
 
   KColorSchemeManager::instance();
 
-  //QApplication::setStyle(QStringLiteral("breeze"));
-  if (qEnvironmentVariableIsEmpty("QT_QUICK_CONTROLS_STYLE")) {
+  // set default style and icon theme
+  QIcon::setFallbackThemeName(QStringLiteral("breeze"));
+  if (qEnvironmentVariableIsEmpty("QT_QUICK_CONTROLS_STYLE") && QQuickStyle::name().isEmpty()) {
     QQuickStyle::setStyle(QStringLiteral("org.kde.desktop"));
   }
+
 
   // Parsing command line options
 
