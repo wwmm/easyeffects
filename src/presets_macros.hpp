33802386d5f1e43b3833a3b8a33b44203cd5e581
wwmm <wellingtonwallace@gmail.com>
2025-12-07 17:10:43 -0300

    Use default values when a preset subsection is not there. It is better than throwing an exception and aborting the preset loading.

--- src/presets_macros.hpp
+++ src/presets_macros.hpp
@@ -6,10 +6,14 @@
     settings->set##property(json.at(section).at(instance_name).value(key, settings->default##property##Value())); \
   }
 
-#define UPDATE_PROPERTY_INSIDE_SUBSECTION(json_section, key, property)                                         \
-  {                                                                                                            \
-    settings->set##property(                                                                                   \
-        json.at(section).at(instance_name).at(json_section).value(key, settings->default##property##Value())); \
+#define UPDATE_PROPERTY_INSIDE_SUBSECTION(json_section, key, property)                                             \
+  {                                                                                                                \
+    const auto& instance_section = json.at(section).at(instance_name);                                             \
+    if (instance_section.contains(json_section)) {                                                                 \
+      settings->set##property(instance_section.at(json_section).value(key, settings->default##property##Value())); \
+    } else {                                                                                                       \
+      settings->set##property(settings->default##property##Value());                                               \
+    }                                                                                                              \
   }
 
 #define UPDATE_ENUM_LIKE_PROPERTY(key, property)                                                                   \
@@ -21,12 +25,15 @@
     }                                                                                                              \
   }
 
-#define UPDATE_ENUM_LIKE_PROPERTY_INSIDE_SUBSECTION(json_section, key, property) \
-  {                                                                              \
-    if (const auto idx = settings->default##property##LabelsValue().indexOf(     \
-            json.at(section).at(instance_name).at(json_section).value(key, "")); \
-        idx != -1) {                                                             \
-      settings->set##property(idx);                                              \
-    }                                                                            \
+#define UPDATE_ENUM_LIKE_PROPERTY_INSIDE_SUBSECTION(json_section, key, property)                                    \
+  {                                                                                                                 \
+    const auto& instance_section = json.at(section).at(instance_name);                                              \
+    if (instance_section.contains(json_section)) {                                                                  \
+      if (const auto idx =                                                                                          \
+              settings->default##property##LabelsValue().indexOf(instance_section.at(json_section).value(key, "")); \
+          idx != -1) {                                                                                              \
+        settings->set##property(idx);                                                                               \
+      }                                                                                                             \
+    }                                                                                                               \
   }
 // NOLINTEND(bugprone-macro-parentheses,cppcoreguidelines-macro-usage)
