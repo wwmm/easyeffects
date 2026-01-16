946b9129e49eb5336c3401d00332e227205ac466
Torge Matthies <openglfreak@googlemail.com>
2026-01-17 00:04:30 +0100

    gate: initialize on main thread

--- src/gate.cpp
+++ src/gate.cpp
@@ -19,6 +19,7 @@
 
 #include "gate.hpp"
 #include <qtypes.h>
+#include <QApplication>
 #include <spa/utils/defs.h>
 #include <algorithm>
 #include <format>
@@ -112,14 +113,29 @@ void Gate::reset() {
 }
 
 void Gate::setup() {
+  std::scoped_lock<std::mutex> lock(data_mutex);
+
   if (!lv2_wrapper->found_plugin) {
     return;
   }
 
+  ready = false;
+
   lv2_wrapper->set_n_samples(n_samples);
 
   if (lv2_wrapper->get_rate() != rate) {
-    lv2_wrapper->create_instance(rate);
+    // NOLINTBEGIN(clang-analyzer-cplusplus.NewDeleteLeaks)
+    QMetaObject::invokeMethod(
+        QApplication::instance(),
+        [this] {
+          lv2_wrapper->create_instance(rate);
+
+          std::scoped_lock<std::mutex> lock(data_mutex);
+
+          ready = true;
+        },
+        Qt::QueuedConnection);
+    // NOLINTEND(clang-analyzer-cplusplus.NewDeleteLeaks)
   }
 }
 
@@ -134,9 +150,22 @@ void Gate::process(std::span<float>& left_in,
                    std::span<float>& right_out,
                    std::span<float>& probe_left,
                    std::span<float>& probe_right) {
-  if (!lv2_wrapper->found_plugin || !lv2_wrapper->has_instance() || bypass) {
+  std::scoped_lock<std::mutex> lock(data_mutex);
+
+  if (bypass) {
     std::ranges::copy(left_in, left_out.begin());
     std::ranges::copy(right_in, right_out.begin());
+
+    return;
+  }
+
+  if (!ready) {
+    std::ranges::copy(left_in, left_out.begin());
+    std::ranges::copy(right_in, right_out.begin());
+
+    if (output_gain != 1.0F) {
+      apply_gain(left_out, right_out, output_gain);
+    }
 
     return;
   }
