236a50aefe2b77e4d5163bda659c469258520cf8
wwmm <wellingtonwallace@gmail.com>
2026-02-18 18:11:55 -0300

    Fixing a clang-tidy warning

--- src/local_server.cpp
+++ src/local_server.cpp
@@ -110,7 +110,7 @@ void LocalServer::onReadyRead() {
 
         util::str_to_num(std::string(matches[1]), state);
 
-        DbMain::setBypass(state == 0 ? false : true);
+        DbMain::setBypass(state != 0);
       }
     } else if (std::strncmp(buf, tags::local_server::load_preset, strlen(tags::local_server::load_preset)) == 0) {
       std::string msg = buf;
