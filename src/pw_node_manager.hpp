0acad7bb768579599fb3132792f6b2107e8fef73
wwmm <wellingtonwallace@gmail.com>
2026-03-08 13:32:52 -0300

    Blocklisting KDE plasma volume level meters

--- src/pw_node_manager.hpp
+++ src/pw_node_manager.hpp
@@ -116,7 +116,7 @@ class NodeManager : public QObject {
       std::to_array({"Easy Effects", "EasyEffects", "easyeffects", "pwvucontrol-peak-detect", "speech-dispatcher",
                      "speech-dispatcher-dummy", "speech-dispatcher-espeak-ng"});
 
-  std::array<std::string, 2U> blocklist_app_id = {"org.PulseAudio.pavucontrol"};
+  std::array<std::string, 2U> blocklist_app_id = {"org.PulseAudio.pavucontrol", "org.kde.plasma-pa"};
 
   std::array<std::string, 2U> blocklist_media_role = {"event", "Notification"};
 
