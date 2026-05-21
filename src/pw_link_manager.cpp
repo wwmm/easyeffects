26e2723f61a42c6cbafe56bc298b98c96b2c5d51
wwmm <wellingtonwallace@gmail.com>
2026-05-20 21:26:49 -0300

    Improved probe port handling

--- src/pw_link_manager.cpp
+++ src/pw_link_manager.cpp
@@ -327,15 +327,18 @@ auto LinkManager::find_matching_ports(const std::vector<PortInfo>& output_ports,
   bool use_audio_channel = true;
 
   // Determine if we should use audio channel matching
+
+  auto handled_audio_channels = std::vector<std::string>{"FL", "FR", "MONO", "PROBE_FL", "PROBE_FR"};
+
   if (!probe_link) {
     for (const auto& port : output_ports) {
-      if (port.audio_channel != "FL" && port.audio_channel != "FR" && port.audio_channel != "MONO") {
+      if (std::ranges::find(handled_audio_channels, port.audio_channel) == handled_audio_channels.end()) {
         use_audio_channel = false;
         break;
       }
     }
     for (const auto& port : input_ports) {
-      if (port.audio_channel != "FL" && port.audio_channel != "FR" && port.audio_channel != "MONO") {
+      if (std::ranges::find(handled_audio_channels, port.audio_channel) == handled_audio_channels.end()) {
         use_audio_channel = false;
         break;
       }
@@ -348,7 +351,9 @@ auto LinkManager::find_matching_ports(const std::vector<PortInfo>& output_ports,
 
       if (!probe_link) {
         if (use_audio_channel) {
-          if (outp.audio_channel == "MONO" || inp.audio_channel == "MONO") {
+          if (inp.audio_channel == "PROBE_FL" || inp.audio_channel == "PROBE_FR") {
+            ports_match = false;  // ignore probe ports when matching regular ports
+          } else if (outp.audio_channel == "MONO" || inp.audio_channel == "MONO") {
             ports_match = true;  // We assume all ports match with mono
           } else {
             ports_match = outp.audio_channel == inp.audio_channel;
@@ -358,8 +363,6 @@ auto LinkManager::find_matching_ports(const std::vector<PortInfo>& output_ports,
           ports_match = outp.port_id == inp.port_id;
         }
       } else {
-        util::warning(std::format("port name = {}", outp.audio_channel));
-
         if ((outp.audio_channel == "MONO" && inp.audio_channel == "PROBE_FL") ||
             (outp.audio_channel == "MONO" && inp.audio_channel == "PROBE_FR") ||
             (outp.audio_channel == "FL" && inp.audio_channel == "PROBE_FL") ||
